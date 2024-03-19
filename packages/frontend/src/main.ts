f380ac0865311aeab1a7487113e800720f4933cf
Paolo Guerra <pg.guerrap@proton.me>
2024-03-19 14:35:47 +0100

    refactor(frontend): remove carrot libs

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -4,25 +4,10 @@ import "@fontsource/inter/700.css";
 import "./main.css";
 
 import { createApp } from "vue";
-import {
-    CarrotVueAdapter,
-    type CarrotWagmiConfig,
-} from "@carrot-kpi/vue-adapter";
 import App from "./App.vue";
 import { router } from "./router";
 import { i18n } from "./i18n";
-import { createConfig, http, injected } from "@wagmi/core";
 import { createPinia } from "pinia";
-import { METROM_CHAIN, SUPPORTED_CHAIN } from "./commons";
-import { ChainId } from "./types";
-
-const wagmiConfig: CarrotWagmiConfig = createConfig({
-    chains: [METROM_CHAIN[ChainId.Gnosis]],
-    transports: {
-        [SUPPORTED_CHAIN[ChainId.Gnosis].id]: http(),
-    },
-    connectors: [injected()],
-});
 
 const pinia = createPinia();
 
@@ -30,10 +15,6 @@ const app = createApp(App, {});
 
 app.use(router);
 app.use(i18n);
-app.use(CarrotVueAdapter, {
-    type: "host",
-    wagmiConfig,
-    pinia,
-});
+app.use(pinia);
 
 app.mount("#app");
