e1b4341dc26fd1f03eec54d404969aaa5a0acfe5
Paolo Guerra <pg.guerrap@proton.me>
2024-03-19 14:35:47 +0100

    refactor(frontend): remove carrot libs

--- packages/frontend/src/commons.ts
+++ packages/frontend/src/commons.ts
@@ -1,25 +1,7 @@
 import { defineChain, type ChainFormatters } from "viem";
-import {
-    Environment,
-    type ServiceUrls,
-    type SupportedChain,
-} from "@carrot-kpi/sdk";
 import { celo, gnosis } from "viem/chains";
 import GnosisLogo from "./icons/GnosisLogoIcon.vue";
-import { ChainId, type MetromChain } from "./types";
-
-export const SERVICE_URLS: Record<Environment, ServiceUrls> = {
-    [Environment.Development]: {
-        staticCdn: "https://static.dev.carrot.community",
-        dataCdn: "https://data.dev.carrot.community",
-        dataManager: "https://data-manager.api.dev.carrot.community",
-    },
-    [Environment.Staging]: {
-        staticCdn: "https://static.staging.carrot.community",
-        dataCdn: "https://data.staging.carrot.community",
-        dataManager: "https://data-manager.api.staging.carrot.community",
-    },
-};
+import { ChainId, type MetromChain, type SupportedChain } from "./types";
 
 // FIXME: move this configuration to the sdk
 export const SUPPORTED_CHAIN: Record<ChainId, SupportedChain> = {
@@ -32,13 +14,9 @@ export const SUPPORTED_CHAIN: Record<ChainId, SupportedChain> = {
             kpiTokensManager: { address: "0x" },
             oraclesManager: { address: "0x" },
         },
-        environment: Environment.Development,
-        serviceUrls: {
-            ...SERVICE_URLS[Environment.Staging],
-            // FIXME: use proper subgraph url
-            subgraph:
-                "https://api.thegraph.com/subgraphs/name/carrot-kpi/carrot-gnosis",
-        },
+        // FIXME: use proper subgraph url
+        subgraphUrl:
+            "https://api.thegraph.com/subgraphs/name/carrot-kpi/carrot-gnosis",
     }),
     [ChainId.Celo]: defineChain<ChainFormatters, SupportedChain>({
         ...celo,
@@ -49,13 +27,9 @@ export const SUPPORTED_CHAIN: Record<ChainId, SupportedChain> = {
             kpiTokensManager: { address: "0x" },
             oraclesManager: { address: "0x" },
         },
-        environment: Environment.Development,
-        serviceUrls: {
-            ...SERVICE_URLS[Environment.Development],
-            // FIXME: use proper subgraph url
-            subgraph:
-                "https://api.thegraph.com/subgraphs/name/carrot-kpi/carrot-celo",
-        },
+        // FIXME: use proper subgraph url
+        subgraphUrl:
+            "https://api.thegraph.com/subgraphs/name/carrot-kpi/carrot-celo",
     }),
 };
 
--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -1,9 +1,28 @@
-import type { SupportedChain } from "@carrot-kpi/sdk";
+import type { Chain, ChainContract } from "viem";
 import type { Component } from "vue";
+
+export enum Environment {
+    Local = "local",
+    Development = "development",
+    Staging = "staging",
+    Production = "production",
+}
 
 export interface ChainIconData {
     logo: Component;
     backgroundColor: string;
+}
+
+export interface SupportedChain extends Chain {
+    contracts: {
+        ensRegistry?: ChainContract;
+        ensUniversalResolver?: ChainContract;
+        multicall3: ChainContract;
+        factory: ChainContract;
+        kpiTokensManager: ChainContract;
+        oraclesManager: ChainContract;
+    };
+    subgraphUrl?: string;
 }
 
 export interface MetromChain extends SupportedChain {
