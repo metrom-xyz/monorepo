97993c81d9a9669d619e7b8ab02873b3f7c2dce1
Paolo Guerra <pg.guerrap@proton.me>
2024-03-21 15:02:16 +0100

    refactor(frontend): add supported amm mapping, use basic chains with wagmi config

--- packages/frontend/src/commons.ts
+++ packages/frontend/src/commons.ts
@@ -1,39 +1,49 @@
-import { defineChain, type ChainFormatters, type Transport, http } from "viem";
+import {
+    defineChain,
+    type ChainFormatters,
+    type Transport,
+    http,
+    type Chain,
+} from "viem";
 import { celoAlfajores } from "viem/chains";
-import GnosisLogo from "./icons/GnosisLogoIcon.vue";
-import { type MetromChain, type SupportedChain } from "./types";
+import { type Amm, type SupportedChain } from "./types";
 import {
     ADDRESS,
     SupportedChain as MetromChainId,
 } from "@metrom-xyz/contracts";
+import UniswapLogoIcon from "./icons/UniswapLogoIcon.vue";
+import GnosisLogoIcon from "./icons/GnosisLogoIcon.vue";
 
-// FIXME: move this configuration to the sdk
-export const SUPPORTED_CHAIN: Record<MetromChainId, SupportedChain> = {
+export const SUPPORTED_AMM: Record<MetromChainId, Amm> = {
+    [MetromChainId.CeloAlfajores]: {
+        logo: UniswapLogoIcon,
+        name: "Uniswap v3",
+        subgraphUrl:
+            "https://api.studio.thegraph.com/query/68570/metrom-uni-v3-celo-alfajores/version/latest",
+    },
+};
+
+export const METROM_CHAIN: Record<MetromChainId, SupportedChain> = {
     [MetromChainId.CeloAlfajores]: defineChain<ChainFormatters, SupportedChain>(
         {
             ...celoAlfajores,
+            icon: {
+                // FIXME: use celo logo
+                logo: GnosisLogoIcon,
+                backgroundColor: "#213147",
+            },
             contracts: {
                 ...celoAlfajores.contracts,
                 factory: ADDRESS[MetromChainId.CeloAlfajores],
             },
             subgraphUrl:
                 "https://api.studio.thegraph.com/query/68570/metrom-celo-alfajores/version/latest",
-            ammSubgraphs: [],
         },
     ),
 };
 
-export const SUPPORTED_CHAINS: [MetromChain, ...MetromChain[]] = [
-    {
-        ...SUPPORTED_CHAIN[MetromChainId.CeloAlfajores],
-        icon: {
-            // FIXME: use celo logo
-            logo: GnosisLogo,
-            backgroundColor: "#213147",
-        },
-    },
-];
+export const SUPPORTED_CHAINS: [Chain, ...Chain[]] = [celoAlfajores];
 
-export const SUPPORTED_CHAIN_TRANSPORT: Record<MetromChainId, Transport> = {
-    [MetromChainId.CeloAlfajores]: http(),
+export const SUPPORTED_CHAIN_TRANSPORT: Record<number, Transport> = {
+    [celoAlfajores.id]: http(),
 };
