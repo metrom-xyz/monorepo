e8272647cf2f159de70995fa5a77044414a8a5b6
Paolo Guerra <pg.guerrap@proton.me>
2024-03-28 16:06:13 +0100

    feat(frontend): disable token if already selected

--- packages/frontend/src/ui/token-select/search/row/MuiTokenSelectSearchRow.vue
+++ packages/frontend/src/ui/token-select/search/row/MuiTokenSelectSearchRow.vue
@@ -9,6 +9,7 @@ defineProps<TokenSelectSearchRowProps>();
     <div
         class="mui_token_select_search_row__root"
         :class="{
+            mui_token_select_search_row__root__disabled: $props.disabled,
             mui_token_select_search_row__root__selected: $props.selected,
         }"
     >
@@ -25,8 +26,12 @@ defineProps<TokenSelectSearchRowProps>();
     @apply flex gap-3 p-3 justify-between items-center rounded-xl hover:cursor-pointer hover:bg-green-light;
 }
 
+.mui_token_select_search_row__root__disabled {
+    @apply opacity-50 pointer-events-none;
+}
+
 .mui_token_select_search_row__root__selected {
-    @apply bg-green-light;
+    @apply opacity-100 bg-green-light;
 }
 
 .mui_token_select_search_row__token_name {
