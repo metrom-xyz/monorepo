7834bb3317e81af2352315045641bc5b268edfd5
Paolo Guerra <pg.guerrap@proton.me>
2024-04-18 16:30:07 +0200

    feat(frontend): make rewards row always editable, add remove icon

--- packages/frontend/src/components/campaign-creation-form/rewards/reward-row/RewardRow.vue
+++ packages/frontend/src/components/campaign-creation-form/rewards/reward-row/RewardRow.vue
@@ -4,11 +4,8 @@ import type { RewardRowProps } from "./types";
 import MuiNumberInput from "@/ui/MuiNumberInput.vue";
 import type { TokenInfo } from "@uniswap/token-lists";
 import { ref, watch } from "vue";
-import MuiRemoteLogo from "@/ui/remote-logo/MuiRemoteLogo.vue";
 import RemoveXIcon from "@/icons/RemoveXIcon.vue";
-import MuiTypography from "@/ui/typography/MuiTypography.vue";
 import { useAttrs } from "vue";
-import MuiTextField from "@/ui/text-field/MuiTextField.vue";
 
 const props = defineProps<RewardRowProps>();
 const tokenModel = defineModel<TokenInfo>("token");
@@ -17,7 +14,6 @@ const amountModel = defineModel<number>("amount");
 const attrs = useAttrs();
 
 const open = ref<boolean>();
-const amountInputActive = ref(false);
 const tokenError = ref(false);
 const amountError = ref(false);
 
@@ -46,7 +42,6 @@ function handleRewardOnTokenRemove() {
     <div class="reward_row__root">
         <div class="reward_row__token__select__wrapper">
             <MuiTokenSelect
-                v-if="!tokenModel"
                 v-model="tokenModel"
                 :open="open"
                 :error="tokenError"
@@ -71,51 +66,28 @@ function handleRewardOnTokenRemove() {
                 @dismiss="open = undefined"
                 @click="open = true"
             />
-            <div v-else class="reward_row__input__selected">
-                <RemoveXIcon
-                    class="reward_row__remove__icon"
-                    @click="handleRewardOnTokenRemove"
-                />
-                <MuiRemoteLogo
-                    :address="tokenModel.address"
-                    lg
-                    class="reward_row__logo__icon"
-                />
-                <MuiTypography>
-                    {{ tokenModel.symbol }}
-                </MuiTypography>
-            </div>
         </div>
         <div class="reward_row__token__amount__input__wrapper">
             <MuiNumberInput
-                v-if="!amountModel || amountInputActive"
                 :placeholder="$t('campaign.rewards.amount')"
                 class="reward_row__token__amount__input"
                 v-model="amountModel"
-                @focus="amountInputActive = true"
-                @blur="amountInputActive = false"
                 :error="amountError"
             />
-            <div
-                v-else
-                class="reward_row__input__selected reward_row__amount__input__selected"
-            >
-                <RemoveXIcon
-                    @click="amountModel = undefined"
-                    class="reward_row__remove__amount__icon"
-                />
-                <MuiTextField
-                    :label="$t('campaign.rewards.amount')"
-                    :value="amountModel.toLocaleString()"
-                    class="reward_row__token__amount__input"
-                />
-            </div>
         </div>
+        <RemoveXIcon
+            class="reward_row__remove__icon"
+            :class="{
+                reward_row__remove__icon_hidden:
+                    $props.rewards.length === 1 && $attrs.index === 0,
+            }"
+            @click="handleRewardOnTokenRemove"
+        />
     </div>
 </template>
 <style>
 .reward_row__root {
-    @apply flex gap-2;
+    @apply flex gap-2 items-center;
 }
 
 .reward_row__token__select__wrapper {
@@ -124,6 +96,14 @@ function handleRewardOnTokenRemove() {
 
 .reward_row__token__amount__input__wrapper {
     @apply w-3/5;
+}
+
+.reward_row__remove__icon {
+    @apply w-10 h-10 hover:cursor-pointer;
+}
+
+.reward_row__remove__icon_hidden {
+    @apply hidden;
 }
 
 .reward_row__token__amount__input {
@@ -145,25 +125,5 @@ function handleRewardOnTokenRemove() {
 
 .reward_row__amount__input__selected {
     @apply justify-between;
-}
-
-.reward_row__input__selected:hover > .reward_row__remove__icon {
-    @apply h-8 w-8 flex;
-}
-
-.reward_row__input__selected:hover > .reward_row__logo__icon {
-    @apply hidden;
-}
-
-.reward_row__remove__icon {
-    @apply w-8 h-8 hidden;
-}
-
-.reward_row__remove__amount__icon {
-    @apply w-8 h-8 invisible;
-}
-
-.reward_row__amount__input__selected:hover > .reward_row__remove__amount__icon {
-    @apply h-8 w-8 visible;
 }
 </style>
