8c0df7c51181fd83eb85021369d6767e167355cf
Paolo Guerra <pg.guerrap@proton.me>
2024-05-03 18:03:51 +0200

    fix(frontend): make pool link amm specific, add supported amm enum

--- packages/frontend/src/utils/amm.ts
+++ packages/frontend/src/utils/amm.ts
@@ -1,10 +1,20 @@
 import { CHAIN_DATA } from "@/commons";
-import type { Amm } from "@/types";
-import type { SupportedChain } from "sdk";
+import { SupportedAmm, type Amm } from "@/types";
+import { SupportedChain, type Pair } from "sdk";
 
 export const getAmm = (
     chainId: SupportedChain,
     ammSlug: string,
 ): Amm | undefined => {
     return CHAIN_DATA[chainId].amms.find((amm) => amm.slug === ammSlug);
+};
+
+export const getPoolLink = (amm: Amm, pair: Pair): string => {
+    if (amm.slug === SupportedAmm.AlgebraIntegral)
+        return amm.addLiquidityUrl.replace("{target_pair}", `${pair.address}`);
+
+    return amm.addLiquidityUrl.replace(
+        "{target_pair}",
+        `${pair.token0.address}/${pair.token1.address}`,
+    );
 };
