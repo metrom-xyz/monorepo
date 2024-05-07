710cec5fe3d1f9c389355f4ef9c6cdcef3e754b9
Paolo Guerra <pg.guerrap@proton.me>
2024-05-07 14:40:41 +0200

    refactor(frontend): add MultiChainLinks component to handle supported or unsupported chains

--- packages/frontend/src/utils/chain.ts
+++ packages/frontend/src/utils/chain.ts
@@ -1,5 +1,5 @@
 import { SUPPORTED_CHAINS } from "@/commons";
 
-export const isChainSupported = (chainId: number) => {
-    return SUPPORTED_CHAINS.find((chain) => chain.id === chainId);
+export const isChainSupported = (chainId: number | string) => {
+    return SUPPORTED_CHAINS.find((chain) => chain.id === Number(chainId));
 };
