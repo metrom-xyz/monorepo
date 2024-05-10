b4d738aa65751bdbc45b6ce2f3afd68c1ef2e3ea
Paolo Guerra <pg.guerrap@proton.me>
2024-05-10 15:02:55 +0200

    feat(sdk): improve fetch campaigns performances

--- packages/sdk/src/subgraph/metrom.ts
+++ packages/sdk/src/subgraph/metrom.ts
@@ -24,7 +24,7 @@ export class MetromSubgraphClient {
 
     async fetchCampaigns(params?: FetchCampaignsParams): Promise<Campaign[]> {
         let lastId = null;
-        const campaigns = [];
+        let campaigns = [];
         do {
             const result: GetCampaignsQueryResult = await query(
                 this.url,
@@ -32,40 +32,42 @@ export class MetromSubgraphClient {
                 { limit: params?.limit || PAGE_SIZE, lastId: lastId || "" },
             );
 
-            for (const campaign of result.campaigns) {
-                const resolvedAmm = await resolveAmmSubgraphClient(
-                    campaign.pool,
-                    this.ammClients,
-                );
+            campaigns = (await Promise.all(
+                result.campaigns.map(async (campaign) => {
+                    const resolvedAmm = await resolveAmmSubgraphClient(
+                        campaign.pool,
+                        this.ammClients,
+                    );
 
-                campaigns.push(<Campaign>{
-                    id: campaign.id,
-                    timestamp: campaign.transaction.timestamp,
-                    amm: resolvedAmm.slug,
-                    chainId: resolvedAmm.chain,
-                    pairChainId: Number(campaign.chainId),
-                    from: campaign.from,
-                    to: campaign.to,
-                    specification: campaign.specification,
-                    root: campaign.root,
-                    data: campaign.data,
-                    pair: await resolvedAmm.fetchPair({
-                        address: campaign.pool,
-                    }),
-                    rewards: campaign.rewards.map((reward) => ({
-                        id: reward.id,
-                        token: {
-                            chainId: resolvedAmm.chain,
-                            address: reward.token.address,
-                            decimals: parseInt(reward.token.decimals),
-                            name: reward.token.name,
-                            symbol: reward.token.symbol,
-                        },
-                        amount: reward.amount,
-                        unclaimed: reward.unclaimed,
-                    })),
-                });
-            }
+                    return {
+                        id: campaign.id,
+                        timestamp: campaign.transaction.timestamp,
+                        amm: resolvedAmm.slug,
+                        chainId: resolvedAmm.chain,
+                        pairChainId: Number(campaign.chainId),
+                        from: campaign.from,
+                        to: campaign.to,
+                        specification: campaign.specification,
+                        root: campaign.root,
+                        data: campaign.data,
+                        pair: await resolvedAmm.fetchPair({
+                            address: campaign.pool,
+                        }),
+                        rewards: campaign.rewards.map((reward) => ({
+                            id: reward.id,
+                            token: {
+                                chainId: resolvedAmm.chain,
+                                address: reward.token.address,
+                                decimals: parseInt(reward.token.decimals),
+                                name: reward.token.name,
+                                symbol: reward.token.symbol,
+                            },
+                            amount: reward.amount,
+                            unclaimed: reward.unclaimed,
+                        })),
+                    };
+                }),
+            )) as Campaign[];
 
             lastId =
                 result.campaigns.length < PAGE_SIZE
