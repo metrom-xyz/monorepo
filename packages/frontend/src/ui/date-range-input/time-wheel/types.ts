de0ad430f9ec168740a7efca1e85a44e07a2157c
Paolo Guerra <pg.guerrap@proton.me>
2024-05-27 16:23:31 +0200

    refactor(frontend): remove unused calendar components

--- packages/frontend/src/ui/date-range-input/time-wheel/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Dayjs } from "dayjs";
-
-export interface DateRangeInputTimeWheelProps {
-    value?: Dayjs | Date | null;
-    min?: Dayjs | Date | null;
-    max?: Dayjs | Date | null;
-}
