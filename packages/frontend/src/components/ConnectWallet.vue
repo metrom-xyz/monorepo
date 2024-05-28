bda39d1b3dd83e4d2994f5fcf1bc5db6b57312de
Paolo Guerra <pg.guerrap@proton.me>
2024-05-28 17:14:34 +0200

    feat(frontend): add connect wallet component

---
+++ packages/frontend/src/components/ConnectWallet.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MuiButton from "@/ui/button/MuiButton.vue";
+import MuiTypography from "@/ui/typography/MuiTypography.vue";
+import { injected } from "@wagmi/core";
+import { useConnect } from "vevm";
+
+const { connect } = useConnect();
+</script>
+<template>
+    <MuiButton @click="connect({ connector: injected() })">
+        <MuiTypography uppercase>{{ $t("wallet.connect") }}</MuiTypography>
+    </MuiButton>
+</template>
+<style></style>
