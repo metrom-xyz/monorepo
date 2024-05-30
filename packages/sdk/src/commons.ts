5cff5455dc41c24cf37d83136be2250c3943f5ee
Paolo Guerra <pg.guerrap@proton.me>
2024-05-30 17:01:39 +0200

    feat(sdk): add service urls mapping, add environments

--- packages/sdk/src/commons.ts
+++ packages/sdk/src/commons.ts
@@ -11,3 +11,29 @@ export enum SupportedAmm {
     Univ3 = "uni-v3",
     TestIntegral = "test-integral",
 }
+
+export enum Environment {
+    Development = "development",
+    Staging = "staging",
+    Production = "production",
+}
+
+export interface ServiceUrls {
+    dataManager: string;
+    metrom: string;
+}
+
+export const SERVICE_URLS: Record<Environment, ServiceUrls> = {
+    [Environment.Development]: {
+        dataManager: "https://data-manager.dev.metrom.xyz",
+        metrom: "https://api.dev.metrom.xyz",
+    },
+    [Environment.Staging]: {
+        dataManager: "https://data-manager.staging.metrom.xyz",
+        metrom: "https://api.staging.metrom.xyz",
+    },
+    [Environment.Production]: {
+        dataManager: "https://data-manager.metrom.xyz",
+        metrom: "https://api.metrom.xyz",
+    },
+};
