fba02f4a9b03f1ad4c40f756438e085e04d713bb
luzzifoss <fedeluzzi00@gmail.com>
2024-06-07 14:22:16 +0100

    feat(algebra-v1.9-subgraph): add liquidity to pool entity and handle same-block positions

--- packages/algebra-v1.9-subgraph/src/mappings/factory.ts
+++ packages/algebra-v1.9-subgraph/src/mappings/factory.ts
@@ -1,14 +1,15 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import { Pool as PoolCreatedEvent } from "../../generated/Factory/Factory";
 import { Pool as PoolTemplate } from "../../generated/templates";
 import { Pool } from "../../generated/schema";
 import { getOrCreateToken } from "../commons";
-import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handlePoolCreated(event: PoolCreatedEvent): void {
     let pool = new Pool(event.params.pool);
     // will be updated in the pool's initialize event
     pool.token0 = getOrCreateToken(event.params.token0).id;
     pool.token1 = getOrCreateToken(event.params.token1).id;
+    pool.liquidity = BigInt.zero();
     pool.tick = BigInt.zero();
     pool.fee = BigInt.fromU32(100);
     pool.save();
