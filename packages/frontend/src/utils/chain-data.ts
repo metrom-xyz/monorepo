dde25f539aa7a2323fe79b0571458257c84d8186
Paolo Guerra <pg.guerrap@proton.me>
2024-06-12 15:59:26 +0200

    refactor(frontend): replace keys with entries

--- packages/frontend/src/utils/chain-data.ts
+++ packages/frontend/src/utils/chain-data.ts
@@ -2,20 +2,21 @@ import { SUPPORTED_AMMS, SUPPORTED_CHAIN_ICONS } from "@/commons";
 import type { ChainData } from "@/types";
 import { ADDRESS, SupportedChain } from "@metrom-xyz/contracts";
 import { MetromApiClient, SERVICE_URLS } from "sdk";
+import type { ChainContract } from "viem";
 
 export const buildChainData = () => {
-    const environmentChains = Object.keys(
+    const environmentChains = Object.entries(
         ADDRESS[__ENVIRONMENT__],
-    ) as unknown as SupportedChain[];
+    ) as unknown as [SupportedChain, ChainContract][];
 
     return environmentChains.reduce(
-        (chainData, chain) => {
+        (chainData, [chain, contract]) => {
             chainData[chain] = {
                 icon: {
                     logo: SUPPORTED_CHAIN_ICONS[chain],
                     backgroundColor: "#000",
                 },
-                contract: ADDRESS[__ENVIRONMENT__][chain],
+                contract,
                 metromApiClient: new MetromApiClient(
                     SERVICE_URLS[__ENVIRONMENT__].metrom,
                     chain,
