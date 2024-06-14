88bc0bb2f9ca67deb0728b476eb7708a9ad8df79
Paolo Guerra <pg.guerrap@proton.me>
2024-06-14 14:35:33 +0200

    feat(ui): add MuiAvatar and MuiEns components

---
+++ packages/frontend/src/ui/avatar/MuiAvatar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import makeBlockie from "ethereum-blockies-base64";
+import { useEnsAvatar, useEnsName } from "vevm";
+import type { AvatarProps } from "./types";
+
+const props = defineProps<AvatarProps>();
+
+const { ensName } = useEnsName({ address: props.address });
+const { ensAvatar } = useEnsAvatar({ name: ensName.value || "" });
+</script>
+<template>
+    <img :src="ensAvatar || makeBlockie(address)" class="mui_avatar__root" />
+</template>
+<style>
+.mui_avatar__root {
+    @apply h-9 w-9 rounded-full;
+}
+</style>
---
+++ packages/frontend/src/ui/ens/MuiEns.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useEnsName } from "vevm";
+import type { EnsProps } from "./types";
+import MuiTypography from "../typography/MuiTypography.vue";
+import { shortenAddress } from "@/utils/address";
+
+const props = defineProps<EnsProps>();
+
+// FIXME: check why it's always undefined
+const { ensName } = useEnsName({ address: props.address });
+</script>
+<template>
+    <MuiTypography>
+        {{
+            ensName || props.short
+                ? shortenAddress(props.address)
+                : props.address
+        }}
+    </MuiTypography>
+</template>
+<style></style>
