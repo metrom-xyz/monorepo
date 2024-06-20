309ae126a28949451bf73a6885fb24a59b16781f
Paolo Guerra <pg.guerrap@proton.me>
2024-06-20 17:01:40 +0200

    feat(frontend): add param to shortenAddress

--- packages/frontend/src/utils/address.ts
+++ packages/frontend/src/utils/address.ts
@@ -1,5 +1,9 @@
 import type { Address } from "viem";
 
-export const shortenAddress = (address?: Address) => {
-    return address ? `${address.slice(0, 10)}...${address.substring(34)}` : "";
+export const shortenAddress = (address?: Address, long?: boolean) => {
+    if (!address) return "";
+
+    return long
+        ? `${address.slice(0, 10)}...${address.substring(34)}`
+        : `${address.slice(0, 6)}...${address.substring(38)}`;
 };
