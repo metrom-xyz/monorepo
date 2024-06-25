441366763f00fd5babf736bd939d1c9293efc82c
Paolo Guerra <pg.guerrap@proton.me>
2024-06-25 15:30:44 +0200

    fix(frontend): issue with navigation bar

--- packages/frontend/src/components/NavigationBar.vue
+++ packages/frontend/src/components/NavigationBar.vue
@@ -2,72 +2,59 @@
 import { useSelectedChain } from "@/composables/useSelectedChain";
 import DocumentIcon from "@/icons/DocumentIcon.vue";
 import FolderIcon from "@/icons/FolderIcon.vue";
-import { MetTypography, MetTabs, MetTab } from "@metrom-xyz/ui";
-import { RouterLink } from "vue-router";
-import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { MetTab, MetTabs, MetTypography } from "@metrom-xyz/ui";
+import { ref, watch } from "vue";
 
 const chain = useSelectedChain();
+const { currentRoute, push } = useRouter();
 
 const tab = ref(0);
 
 function handleTabChange(value: number | undefined) {
-    console.log({ value });
     if (value === undefined) return;
-    tab.value = value;
+    push(!value ? `/?chain=${chain.value}` : `/create?chain=${chain.value}`);
 }
+
+watch(currentRoute, () => {
+    if (currentRoute.value.name === "create") tab.value = 1;
+    else tab.value = 0;
+});
 </script>
 <template>
     <nav class="navigation_bar__root">
         <div class="navigation_bar__wrapper">
             <MetTabs @change="handleTabChange" :value="tab">
                 <MetTab>
-                    <RouterLink :to="`/?chain=${chain}`">
-                        <div class="navigation_bar__link">
-                            <FolderIcon />
-                            <MetTypography>
-                                {{ $t("navigation.campaigns.all") }}
-                            </MetTypography>
-                        </div>
-                    </RouterLink>
+                    <div class="navigation_bar__link">
+                        <FolderIcon />
+                        <MetTypography>
+                            {{ $t("navigation.campaigns.all") }}
+                        </MetTypography>
+                    </div>
                 </MetTab>
                 <MetTab>
-                    <RouterLink :to="`/create?chain=${chain}`">
-                        <div class="navigation_bar__link">
-                            <DocumentIcon />
-                            <MetTypography>
-                                {{ $t("navigation.campaigns.create") }}
-                            </MetTypography>
-                        </div>
-                    </RouterLink>
+                    <div class="navigation_bar__link">
+                        <DocumentIcon />
+                        <MetTypography>
+                            {{ $t("navigation.campaigns.create") }}
+                        </MetTypography>
+                    </div>
                 </MetTab>
             </MetTabs>
-            <div
-                class="navigation_bar__link__active"
-                :class="{
-                    navigation_bar__link_slide_right: $route.name === 'create',
-                }"
-            ></div>
         </div>
     </nav>
 </template>
 <style>
-/* .navigation_bar__root {
-    @apply h-14 flex bg-gray-100 rounded-xxl p-1.5 absolute left-1/2 -translate-x-1/2;
+.navigation_bar__root {
+    @apply absolute left-1/2 -translate-x-1/2;
 }
 
 .navigation_bar__wrapper {
     @apply relative flex;
-} */
+}
 
 .navigation_bar__link {
     @apply flex items-center gap-2 whitespace-nowrap;
 }
-
-/* .navigation_bar__link__active {
-    @apply -z-[1] absolute h-full w-1/2 bg-white rounded-xl transition-transform duration-200 ease-in-out;
-}
-
-.navigation_bar__link_slide_right {
-    @apply translate-x-full;
-} */
 </style>
