e23e9311751e2d463535eaaf30a32806d9e83aec
luzzif <federico.luzzi@proton.me>
2024-07-02 15:07:55 +0100

    chore: upgrades and turbo setup

--- packages/sdk/eslint.config.js
+++ packages/sdk/eslint.config.js
@@ -1,22 +1,12 @@
 import eslint from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-
-import tsParser from "@typescript-eslint/parser";
-import tsEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 export default [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettier,
     {
-        files: ["src/**/*.ts"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-            parser: tsParser,
-        },
-        plugins: {
-            tsEslint,
-            prettier,
-        },
-        ignores: ["node_modules", "dist", ".turbo", "storybook-static"],
+        ignores: ["dist/**"],
     },
 ];
