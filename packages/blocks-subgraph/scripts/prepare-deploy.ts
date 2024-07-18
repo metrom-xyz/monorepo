0fa24188b2b72aff755f6dc820d0021567440574
luzzif <federico.luzzi@proton.me>
2024-07-18 10:00:37 +0100

    feat: new mantle sepolia subgraphs and new subgraph.yaml generation method

---
+++ packages/blocks-subgraph/scripts/prepare-deploy.ts
@@ -0,0 +1,48 @@
+import { existsSync, writeFileSync, rmSync, readFileSync } from "node:fs";
+import { dirname, join } from "node:path";
+import { START_BLOCKS } from "../start-blocks";
+import { fileURLToPath } from "node:url";
+import { exec } from "node:child_process";
+import Mustache from "mustache";
+
+const [, , rawNetwork = ""] = process.argv;
+const network = rawNetwork.toLowerCase();
+
+const startBlock = START_BLOCKS[network];
+if (!startBlock) {
+    console.error(
+        `"${network}" is not a valid network. Valid values are: ${Object.keys(START_BLOCKS).join(", ")}`,
+    );
+    process.exit(1);
+}
+
+console.log(`Generating subgraph file for network ${network}`);
+
+try {
+    const subgraphFileOut = join(
+        fileURLToPath(dirname(import.meta.url)),
+        "../subgraph.yaml",
+    );
+    if (existsSync(subgraphFileOut)) rmSync(subgraphFileOut);
+    writeFileSync(
+        subgraphFileOut,
+        Mustache.render(
+            readFileSync(
+                join(
+                    fileURLToPath(dirname(import.meta.url)),
+                    "../subgraph.template.yaml",
+                ),
+            ).toString(),
+            {
+                network,
+                startBlock,
+            },
+        ),
+    );
+    console.log("Subgraph file successfully generated.");
+} catch (error) {
+    console.error("Error while generating subgraph file", error);
+    process.exit(1);
+}
+
+exec("pnpm format");
---
+++ packages/blocks-subgraph/start-blocks.ts
@@ -0,0 +1,7 @@
+export const START_BLOCKS: { [network: string]: number } = {
+    "celo-alfajores": 23112411,
+    gnosis: 32892235,
+    holesky: 1468000,
+    "arbitrum-one": 154000000,
+    "mantle-sepolia": 9873155,
+};
