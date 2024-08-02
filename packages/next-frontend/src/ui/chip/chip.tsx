81c497c3fa671295e1bb06c3f8c8a388e0041df2
Paolo Guerra <pg.guerrap@proton.me>
2024-08-02 18:27:30 +0200

    feat(next-frontend): add chip component

---
+++ packages/next-frontend/src/ui/chip/chip.tsx
@@ -0,0 +1,39 @@
+import React, { type HTMLAttributes, type ReactNode } from "react";
+import classNames from "@/src/utils/classes";
+import { Typography } from "../typography";
+
+import styles from "./styles.module.css";
+
+export interface BaseChipsProps {
+    size?: "big" | "small";
+    clickable?: boolean;
+    active?: boolean;
+    className?: { root?: string };
+    children: ReactNode;
+}
+
+export type ChipProps = BaseChipsProps &
+    Omit<HTMLAttributes<HTMLDivElement>, keyof BaseChipsProps>;
+
+export const Chip = ({
+    size = "small",
+    clickable,
+    active,
+    children,
+    className,
+    ...rest
+}: ChipProps) => (
+    <div
+        {...rest}
+        className={classNames(styles.root, {
+            [styles.rootSmall]: size === "small",
+            [styles.rootBig]: size === "big",
+            [styles.rootClickable]: clickable,
+            [styles.rootActive]: active,
+        })}
+    >
+        <Typography variant="sm" weight="medium">
+            {children}
+        </Typography>
+    </div>
+);
