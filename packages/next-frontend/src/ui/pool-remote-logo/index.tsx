f777c29c32a3ce9a031fae6c5efb387ad214819f
Paolo Guerra <pg.guerrap@proton.me>
2024-08-05 16:18:21 +0200

    feat(next-frontend): implement token and pool logo components

---
+++ packages/next-frontend/src/ui/pool-remote-logo/index.tsx
@@ -0,0 +1,32 @@
+import styles from "./styles.module.css";
+import { RemoteLogo, type RemoteLogoProps } from "../remote-logo";
+import classNames from "@/src/utils/classes";
+
+interface PoolRemoteLogoProps {
+    size?: "sm" | "md" | "lg";
+    chain?: number | string;
+    token0?: Omit<RemoteLogoProps, "size" | "chain">;
+    token1?: Omit<RemoteLogoProps, "size" | "chain">;
+    className?: {
+        root?: string;
+    };
+}
+
+export function PoolRemoteLogo({
+    size = "md",
+    chain,
+    token0,
+    token1,
+    className,
+}: PoolRemoteLogoProps) {
+    return (
+        <div
+            className={classNames(className?.root, styles.root, {
+                [styles[size]]: true,
+            })}
+        >
+            <RemoteLogo chain={chain} size={size} {...token0} defaultText=" " />
+            <RemoteLogo chain={chain} size={size} {...token1} defaultText=" " />
+        </div>
+    );
+}
---
+++ packages/next-frontend/src/ui/remote-logo/index.tsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useMemo, useState } from "react";
+import Image from "next/image";
+import { type Address } from "viem";
+import classNames from "@/src/utils/classes";
+import { getAddressColor } from "@/src/utils/address";
+import { Skeleton } from "../skeleton";
+import { Typography } from "../typography";
+
+import styles from "./styles.module.css";
+
+export interface RemoteLogoProps {
+    src?: string;
+    address?: Address;
+    chain?: number | string;
+    size?: "sm" | "md" | "lg";
+    defaultText?: string;
+    className?: { root: string };
+}
+
+const BAD_SRC: Record<string, boolean> = {};
+
+export const RemoteLogo = ({
+    src,
+    address,
+    chain,
+    size = "md",
+    defaultText = "?",
+    className,
+}: RemoteLogoProps) => {
+    const [imageError, setImageError] = useState(false);
+
+    const resolvedSrc = useMemo(() => {
+        if (src) return src;
+        if (!address || !chain) return "";
+        return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${chain}/assets/${address}/logo.png`;
+    }, [address, chain, src]);
+
+    const validResolvedSrc = useMemo(() => {
+        if (!resolvedSrc || BAD_SRC[resolvedSrc] || imageError) return null;
+        return resolvedSrc;
+    }, [resolvedSrc, imageError]);
+
+    const handleImageError = useCallback(() => {
+        if (resolvedSrc) {
+            BAD_SRC[resolvedSrc] = true;
+            setImageError(true);
+        }
+    }, [resolvedSrc]);
+
+    if (validResolvedSrc) {
+        return (
+            <div
+                className={classNames(className?.root, styles.root, {
+                    [styles[size]]: true,
+                })}
+            >
+                <Skeleton
+                    circular
+                    width="100%"
+                    className={{ root: styles.skeleton }}
+                />
+                <Image
+                    fill
+                    src={validResolvedSrc}
+                    alt={address || ""}
+                    className={classNames(styles.image, {
+                        [styles[size]]: true,
+                    })}
+                    onError={handleImageError}
+                />
+            </div>
+        );
+    }
+
+    return (
+        <div
+            style={{
+                backgroundColor: getAddressColor(address),
+            }}
+            className={classNames(styles.fallback, {
+                [styles[size]]: true,
+            })}
+        >
+            <Typography uppercase className={{ root: styles.fallbackText }}>
+                {!!defaultText
+                    ? defaultText.length > 4
+                        ? `${defaultText.slice(0, 4)}`
+                        : defaultText
+                    : "?"}
+            </Typography>
+        </div>
+    );
+};
