f8bc141a226bcf7a6ad01041c204d58f4df3bb1b
Paolo Guerra <pg.guerrap@proton.me>
2024-08-07 16:55:11 +0200

    fix(next-frontend): scroll into view issue

--- packages/next-frontend/src/ui/scroll-into-view.tsx
+++ packages/next-frontend/src/ui/scroll-into-view.tsx
@@ -9,12 +9,25 @@ export function ScrollIntoView({ selected, children }: ScrollIntoViewProps) {
     const listItemRef = useRef<HTMLElement>(null);
 
     useEffect(() => {
-        if (selected && listItemRef.current) {
-            listItemRef.current.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
-        }
+        if (!selected || !listItemRef.current) return;
+
+        const container = listItemRef.current.parentElement;
+        if (!container) return;
+
+        const { offsetTop, offsetLeft, clientHeight, clientWidth } =
+            listItemRef.current;
+
+        container.scrollTo({
+            top:
+                offsetTop -
+                clientHeight / 2 +
+                listItemRef.current.offsetHeight / 2,
+            left:
+                offsetLeft -
+                clientWidth / 2 +
+                listItemRef.current.offsetWidth / 2,
+            behavior: "smooth",
+        });
     }, [selected]);
 
     return <span ref={listItemRef}>{children}</span>;
