36f53d2df48050530b6a5432857c2c088f655dc1
Paolo Guerra <pg.guerrap@proton.me>
2024-08-07 17:37:30 +0200

    feat(next-frontend): add basic structure for rewards step

--- packages/next-frontend/src/components/create-campaign/form/index.tsx
+++ packages/next-frontend/src/components/create-campaign/form/index.tsx
@@ -1,9 +1,10 @@
 import { useTranslations } from "next-intl";
 import type { CampaignPayload, CampaignPayloadPart } from "@/src/types";
 import { AmmStep } from "./amm-step";
+import { PoolStep } from "./pool-step";
 import { StartDateStep } from "./start-date-step";
 import { EndDateStep } from "./end-date-step";
-import { PoolStep } from "./pool-step";
+import { RewardsStep } from "./rewards-step";
 
 import styles from "./styles.module.css";
 
@@ -38,6 +39,11 @@ export function CreateCampaignForm({
                 startDate={payload?.startDate}
                 endDate={payload?.endDate}
                 onEndDateChange={onPayloadChange}
+            />
+            <RewardsStep
+                disabled={!payload?.endDate}
+                rewards={payload?.rewards}
+                onRewardsChange={onPayloadChange}
             />
         </div>
     );
