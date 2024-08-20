780021da5bb529601fa8b119d88c2b3e701afaa0
Paolo Guerra <pg.guerrap@proton.me>
2024-08-20 18:08:28 +0200

    feat(frontend): add error text component

---
+++ packages/frontend/src/ui/error-text/index.tsx
@@ -0,0 +1,17 @@
+import { ErrorIcon } from "@/src/assets/error-icon";
+import { Typography, type TypographyProps } from "../typography";
+
+import styles from "./styles.module.css";
+
+interface ErrorTextProps extends TypographyProps {}
+
+export function ErrorText({ children, ...rest }: ErrorTextProps) {
+    return (
+        <div className={styles.root}>
+            <ErrorIcon className={styles.icon} />
+            <Typography uppercase {...rest} className={{ root: styles.text }}>
+                {children}
+            </Typography>
+        </div>
+    );
+}
