2824e172d5d1bd7bb50c7da25ddbbab688505f97
Federico Luzzi <federico.luzzi@proton.me>
2024-08-21 17:11:40 +0200

    feat(frontend): networks switcher (#124)
    * feat(frontend): networks switcher
    
    * chore: address pr comments

---
+++ packages/frontend/src/ui/popover/index.tsx
@@ -0,0 +1,71 @@
+import {
+    forwardRef,
+    type ReactElement,
+    type ReactNode,
+    useState,
+    useEffect,
+} from "react";
+import { usePopper } from "react-popper";
+import { type Placement } from "@popperjs/core";
+
+import styles from "./styles.module.css";
+
+export interface PopoverProps {
+    open: boolean;
+    anchor?: Element | null;
+    placement?: Placement;
+    offset?: [number, number];
+    className?: string;
+    children?: ReactNode;
+}
+
+export const Popover = forwardRef<HTMLDivElement, PopoverProps>(
+    function Popover(
+        {
+            open,
+            anchor,
+            placement = "auto",
+            offset = [0, 8],
+            className,
+            children,
+        },
+        ref,
+    ): ReactElement {
+        const [popper, setPopper] = useState<HTMLDivElement | null>(null);
+
+        const {
+            styles: popperStyles,
+            attributes,
+            update,
+        } = usePopper(anchor, popper, {
+            placement,
+            modifiers: [
+                {
+                    name: "offset",
+                    options: { offset },
+                },
+            ],
+        });
+
+        useEffect(() => {
+            if (open && update) update();
+        }, [open, update]);
+
+        return (
+            <div
+                ref={(element) => {
+                    if (ref) {
+                        if (typeof ref === "function") ref(element);
+                        else ref.current = element;
+                    }
+                    setPopper(element);
+                }}
+                style={{ ...popperStyles.popper }}
+                className={`${styles.root} ${className} ${open ? styles.open : ""}`}
+                {...attributes.popper}
+            >
+                {children}
+            </div>
+        );
+    },
+);
