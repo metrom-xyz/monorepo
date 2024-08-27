1b0aa8b7b23e2bbfeffa5dc1ab637bc0f79b1b84
luzzif <federico.luzzi@proton.me>
2024-08-27 17:21:52 +0200

    chore(frontend): new component name

--- packages/frontend/src/app/[locale]/claims/page.tsx
+++ packages/frontend/src/app/[locale]/claims/page.tsx
@@ -1,13 +1,13 @@
-import { Claims } from "@/src/components/claims";
+import { Claims as ClaimsPage } from "@/src/components/claims";
 import { Nav } from "@/src/components/nav";
 
 import styles from "../styles.module.css";
 
-export default function CampaignsCreatePage() {
+export default function Claims() {
     return (
         <div className={styles.layout}>
             <Nav />
-            <Claims />
+            <ClaimsPage />
         </div>
     );
 }
