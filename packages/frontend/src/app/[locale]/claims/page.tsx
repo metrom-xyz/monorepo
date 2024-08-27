7479497659734723bb9712832dcef9f20ef05c7c
luzzif <federico.luzzi@proton.me>
2024-08-27 17:35:10 +0200

    chore(frontend): new component name

--- packages/frontend/src/app/[locale]/claims/page.tsx
+++ packages/frontend/src/app/[locale]/claims/page.tsx
@@ -1,13 +1,13 @@
-import { Claims as ClaimsPage } from "@/src/components/claims";
+import { Claims } from "@/src/components/claims";
 import { Nav } from "@/src/components/nav";
 
 import styles from "../styles.module.css";
 
-export default function Claims() {
+export default function ClaimsPage() {
     return (
         <div className={styles.layout}>
             <Nav />
-            <ClaimsPage />
+            <Claims />
         </div>
     );
 }
