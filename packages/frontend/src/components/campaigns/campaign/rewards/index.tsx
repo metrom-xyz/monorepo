1bb9697ca58abdb25760edc74ca938c8b0ba1a15
Divyam Arora <89800407+divyam1705@users.noreply.github.com>
2024-09-10 21:36:28 -0500

    fix(frontend): tooltip new design

--- packages/frontend/src/components/campaigns/campaign/rewards/index.tsx
+++ packages/frontend/src/components/campaigns/campaign/rewards/index.tsx
@@ -10,6 +10,7 @@ import { Popover } from "@/src/ui/popover";
 import { useRef, useState } from "react";
 
 import styles from "./styles.module.css";
+import { useTranslations } from "next-intl";
 
 interface RewardsProps {
     from: number;
@@ -19,6 +20,12 @@ interface RewardsProps {
 }
 
 export function Rewards({ from, to, rewards, chainId }: RewardsProps) {
+    const t = useTranslations("allCampaigns.rewards");
+
+    const totalUsdValue = rewards.reduce((sum, reward) => {
+        return sum + (reward.usdValue || 0);
+    }, 0);
+
     const [popoverOpen, setPopoverOpen] = useState(false);
     const [rewardsBreakdown, setRewardsBreakdown] =
         useState<HTMLDivElement | null>(null);
@@ -47,6 +54,14 @@ export function Rewards({ from, to, rewards, chainId }: RewardsProps) {
                 placement="top"
             >
                 <div className={styles.breakdownContainer}>
+                    <Typography
+                        variant="sm"
+                        className={styles.tooltipTitle}
+                        weight="medium"
+                        uppercase
+                    >
+                        {t("tooltip.rewards")}
+                    </Typography>
                     {rewards.map((reward, i) => {
                         return (
                             <div
@@ -60,24 +75,43 @@ export function Rewards({ from, to, rewards, chainId }: RewardsProps) {
                                         address={reward.address}
                                         defaultText={reward.symbol}
                                     />
-                                    <Typography weight="medium">
+                                    <Typography
+                                        className={styles.tooltipText}
+                                        weight="medium"
+                                        variant="sm"
+                                    >
                                         {reward.symbol}
                                     </Typography>
                                 </div>
                                 <div>
-                                    <Typography weight="medium">
+                                    <Typography
+                                        className={styles.tooltipText}
+                                        weight="medium"
+                                        variant="sm"
+                                    >
                                         {formatTokenAmount(reward.amount)}
-                                    </Typography>
-                                    <Typography weight="medium">
-                                        {reward.usdValue
-                                            ? formatUsdAmount(reward.usdValue)
-                                            : "-"}
                                     </Typography>
                                 </div>
                             </div>
                         );
                     })}
+                    <Typography
+                        className={styles.tooltipTitle}
+                        weight="medium"
+                        variant="sm"
+                        uppercase
+                    >
+                        {t("tooltip.totalUsdValue")}
+                    </Typography>
+                    <Typography
+                        className={styles.tooltipText}
+                        variant="lg"
+                        weight="medium"
+                    >
+                        {totalUsdValue ? formatUsdAmount(totalUsdValue) : "-"}
+                    </Typography>
                 </div>
+                <div className={styles.tooltipTriangle} />
             </Popover>
             <div
                 ref={setRewardsBreakdown}
