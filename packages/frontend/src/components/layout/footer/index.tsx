eaa46031880fd24a4643bae9fc8f09185061a62d
Paolo Guerra <pg.guerrap@proton.me>
2024-09-20 16:51:42 +0200

    feat(frontend): add not found page, minor UI changes

--- packages/frontend/src/components/layout/footer/index.tsx
+++ packages/frontend/src/components/layout/footer/index.tsx
@@ -7,6 +7,7 @@ import { TelegramLogo } from "@/src/assets/logos/socials/telegram";
 import { Typography } from "@metrom-xyz/ui";
 import { useTranslations } from "next-intl";
 import { trackFathomEvent } from "@/src/utils/fathom";
+import { MetromLogo } from "@/src/assets/metrom-logo";
 
 import styles from "./styles.module.css";
 
@@ -15,39 +16,46 @@ export function Footer() {
 
     return (
         <div className={styles.root}>
-            <div className={styles.socials}>
-                <a
-                    href="https://github.com/metrom-xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={() => trackFathomEvent("CLICK_GITHUB_LINK")}
-                >
-                    <GithubLogo className={styles.socialIcon} />
-                </a>
-                <a
-                    href="https://x.com/metromxyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={() => trackFathomEvent("CLICK_X_LINK")}
-                >
-                    <XLogo className={styles.socialIcon} />
-                </a>
-                <a
-                    href="https://discord.com/invite/uRer2D4Pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={() => trackFathomEvent("CLICK_DISCORD_LINK")}
-                >
-                    <DiscordLogo className={styles.socialIcon} />
-                </a>
-                <a
-                    href="https://t.me/metrom_xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={() => trackFathomEvent("CLICK_TELEGRAM_LINK")}
-                >
-                    <TelegramLogo className={styles.socialIcon} />
-                </a>
+            <div className={styles.leftContent}>
+                <div className={styles.socials}>
+                    <a
+                        aria-label={t("socials.github")}
+                        href="https://github.com/metrom-xyz"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => trackFathomEvent("CLICK_GITHUB_LINK")}
+                    >
+                        <GithubLogo className={styles.socialIcon} />
+                    </a>
+                    <a
+                        aria-label={t("socials.x")}
+                        href="https://x.com/metromxyz"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => trackFathomEvent("CLICK_X_LINK")}
+                    >
+                        <XLogo className={styles.socialIcon} />
+                    </a>
+                    <a
+                        aria-label={t("socials.discord")}
+                        href="https://discord.com/invite/uRer2D4Pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => trackFathomEvent("CLICK_DISCORD_LINK")}
+                    >
+                        <DiscordLogo className={styles.socialIcon} />
+                    </a>
+                    <a
+                        aria-label={t("socials.telegram")}
+                        href="https://t.me/metrom_xyz"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => trackFathomEvent("CLICK_TELEGRAM_LINK")}
+                    >
+                        <TelegramLogo className={styles.socialIcon} />
+                    </a>
+                </div>
+                <MetromLogo className={styles.logo} />
             </div>
             <div className={styles.support}>
                 <a
@@ -55,7 +63,7 @@ export function Footer() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <Typography weight="medium">{t("support")}</Typography>
+                    <Typography>{t("support")}</Typography>
                 </a>
                 <a
                     href="https://docs.metrom.xyz"
@@ -63,10 +71,10 @@ export function Footer() {
                     rel="noopener noreferrer"
                     onClick={() => trackFathomEvent("CLICK_DOCUMENTATION_LINK")}
                 >
-                    <Typography light>{t("documentation")}</Typography>
+                    <Typography>{t("documentation")}</Typography>
                 </a>
                 <a href="mailto:hello@metrom.xyz">
-                    <Typography light>{t("contactUs")}</Typography>
+                    <Typography>{t("contactUs")}</Typography>
                 </a>
             </div>
         </div>
