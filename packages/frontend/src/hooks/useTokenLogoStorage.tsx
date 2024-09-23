c640cf2fba9179174ff1fd2b775895cbc0d77a5b
Divyam Arora <89800407+divyam1705@users.noreply.github.com>
2024-09-23 10:07:56 -0500

    fix(frontend): react query and persistent storage

--- packages/frontend/src/hooks/useTokenLogoStorage.tsx
+++ packages/frontend/src/hooks/useTokenLogoStorage.tsx
@@ -1,4 +1,7 @@
-import { useEffect } from "react";
+import { useMemo } from "react";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 interface Token {
     chainId: string;
@@ -13,38 +16,57 @@ const TOKEN_LIST_URLS = [
 ];
 
 const fetchTokenList = async (url: string): Promise<Token[]> => {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data.tokens;
-    } catch (error) {
-        console.error("Error fetching token list:", error);
-        return [];
-    }
-};
-
-const storeTokenLogos = (tokens: Token[]) => {
-    const tokenLogoMap = JSON.parse(localStorage.getItem("tokenLogos") || "{}");
-
-    tokens.forEach((token) => {
-        const key = `${token.address.toLowerCase()}-${token.chainId}`;
-        tokenLogoMap[key] = token.logoURI;
-    });
-    localStorage.setItem("tokenLogos", JSON.stringify(tokenLogoMap));
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.tokens;
 };
 
 export const useTokenLogoStorage = () => {
-    useEffect(() => {
-        const fetchAndStoreLogos = async () => {
-            const allTokens = [];
-
+    const { data: tokenLists } = useQuery({
+        queryKey: ["tokenLists"],
+        queryFn: async () => {
+            const allTokens: Token[] = [];
             for (const url of TOKEN_LIST_URLS) {
                 const tokens = await fetchTokenList(url);
                 allTokens.push(...tokens);
             }
-            storeTokenLogos(allTokens);
-        };
+            return allTokens;
+        },
+        staleTime: 24 * 60 * 60 * 1000,
+    });
 
-        fetchAndStoreLogos();
-    }, []);
+    const tokenLogoMap = useMemo(() => {
+        if (tokenLists) {
+            const tokenMap: Record<string, string> = {};
+            tokenLists.forEach((token) => {
+                const key = `${token.address.toLowerCase()}-${token.chainId}`;
+                tokenMap[key] = token.logoURI;
+            });
+            return tokenMap;
+        }
+    }, [tokenLists]);
+
+    return tokenLogoMap;
 };
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            gcTime: 24 * 60 * 60 * 1000,
+        },
+    },
+});
+
+const localStoragePersistor = createSyncStoragePersister({
+    storage: typeof window !== "undefined" ? window.localStorage : null,
+});
+
+persistQueryClient({
+    queryClient: queryClient,
+    persistor: localStoragePersistor,
+    dehydrateOptions: {
+        shouldDehydrateQuery: (query) => {
+            return query.queryKey[0] === "tokenLists";
+        },
+    },
+});
