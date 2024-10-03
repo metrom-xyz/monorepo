fabad05caf2d76f2f86825ccd308a7dd4ae1b9d9
Paolo Guerra <pg.guerrap@proton.me>
2024-10-03 16:26:10 +0200

    fix(frontend): make drawer wider, use react query to fetch activities

--- packages/frontend/src/hooks/useActivities.ts
+++ packages/frontend/src/hooks/useActivities.ts
@@ -1,8 +1,9 @@
 import { type Activity } from "@metrom-xyz/sdk";
 import { SupportedChain } from "@metrom-xyz/contracts";
-import { useEffect, useState } from "react";
 import { metromApiClient } from "../commons";
 import { useAccount, useChainId } from "wagmi";
+import { useQuery } from "@tanstack/react-query";
+import type { Address } from "viem";
 
 const TIME_RANGE = 60 * 60 * 24 * 7; // 1 week
 
@@ -14,43 +15,31 @@ export function useActivities(): {
     const chainId: SupportedChain = useChainId();
     const { address } = useAccount();
 
-    const [activities, setActivities] = useState<Activity[]>([]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        let cancelled = false;
-
-        async function fetchData() {
-            if (!cancelled) setLoading(false);
-            if (!cancelled) setActivities([]);
-
-            if (!chainId || !address) return;
+    const { data: activities, isPending: loading } = useQuery({
+        queryKey: ["activities", address],
+        initialData: [],
+        queryFn: async ({ queryKey }) => {
+            const account = queryKey[1];
+            if (!account) return [];
 
             try {
-                if (!cancelled) setLoading(true);
-
                 const to = Math.floor(Date.now() / 1000);
                 const activities = await metromApiClient.fetchActivities({
                     chainId,
-                    address,
+                    address: account as Address,
                     from: to - TIME_RANGE,
                     to,
                 });
 
-                if (!cancelled) setActivities(activities);
+                return activities;
             } catch (error) {
-                console.error(
+                throw new Error(
                     `Could not fetch activity for address ${address} in chain id ${chainId}: ${error}`,
                 );
-            } finally {
-                if (!cancelled) setLoading(false);
             }
-        }
-        fetchData();
-        return () => {
-            cancelled = true;
-        };
-    }, [address, chainId]);
+        },
+        enabled: !!chainId && !!address,
+    });
 
     return {
         loading,
