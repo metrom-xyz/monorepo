7a8a8535f919644c42043dc9501394a8fa81a651
luzzif <federico.luzzi@proton.me>
2024-10-08 11:21:08 +0100

    feat(frontend): update copy and multisig connected notification style

--- packages/frontend/src/components/safe-provider/connected-notification.tsx
+++ packages/frontend/src/components/safe-provider/connected-notification.tsx
@@ -1,20 +1,16 @@
 import { useTranslations } from "next-intl";
 import { ToastNotification, Typography } from "@metrom-xyz/ui";
+import { SafeLogo } from "@/src/assets/logos/safe";
 
 interface ConnectedNotificationProps {
     toastId: string | number;
 }
 
 export function ConnectedNotification({ toastId }: ConnectedNotificationProps) {
-    const t = useTranslations("notifications.multisig");
+    const t = useTranslations("notifications.safe");
 
     return (
-        <ToastNotification
-            toastId={toastId}
-            title={t("title")}
-            // TODO: add icons
-            // icon={ClaimReward}
-        >
+        <ToastNotification toastId={toastId} title={t("title")} icon={SafeLogo}>
             <Typography weight="medium">{t("content")}</Typography>
         </ToastNotification>
     );
