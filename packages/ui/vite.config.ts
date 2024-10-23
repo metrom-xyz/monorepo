164ffbbef5d72876c217e6d2c7eb91fdc15c8f35
luzzif <federico.luzzi@proton.me>
2024-10-23 17:12:58 +0100

    chore(ui): explicitly exclude jsx runtime from lib bundle

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             formats: ["es"],
         },
         rollupOptions: {
-            external: Object.keys(peerDependencies),
+            external: [...Object.keys(peerDependencies), "react/jsx-runtime"],
             output: {
                 preserveModules: true,
                 preserveModulesRoot: "src",
