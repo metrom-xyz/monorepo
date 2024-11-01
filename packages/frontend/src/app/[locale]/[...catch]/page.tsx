17075af4a84f812d7b9b8e3e184c4a3078b624ca
Federico Luzzi <federico.luzzi@proton.me>
2024-11-01 10:54:31 +0000

    feat: first implementation of static rendering (#237)

--- packages/frontend/src/app/[locale]/[...catch]/page.tsx
+++ packages/frontend/src/app/[locale]/[...catch]/page.tsx
@@ -1,5 +1,21 @@
+import { routing } from "@/src/i18n/routing";
+import { unstable_setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 
-export default function NotFoundCatchAll() {
+export default function NotFoundCatchAll({
+    params: { locale },
+}: {
+    params: { locale: string };
+}) {
+    if (!routing.locales.includes(locale as any)) {
+        notFound();
+    }
+
+    unstable_setRequestLocale(locale);
+
     notFound();
+}
+
+export function generateStaticParams() {
+    return routing.locales.map((locale) => ({ locale }));
 }
