dac9ad6eca55968e055a4c25234e33c1a21f15c6
Paolo Guerra <pg.guerrap@proton.me>
2024-11-14 16:39:13 +0100

    feat(frontend): add navigation menu on mobile

--- packages/frontend/src/components/layout/nav/index.tsx
+++ packages/frontend/src/components/layout/nav/index.tsx
@@ -4,11 +4,14 @@ import { Link, usePathname } from "@/src/i18n/routing";
 import { MetromSquareLogo } from "@/src/assets/metrom-square-logo";
 import { useTranslations } from "next-intl";
 import { ConnectButton } from "../../connect-button";
-import { Typography } from "@metrom-xyz/ui";
+import { Popover, Typography } from "@metrom-xyz/ui";
 import classNames from "classnames";
 import { NewCampaignIcon } from "@/src/assets/new-campaign-icon";
 import { AllCampaignsIcon } from "@/src/assets/all-campaigns-icon";
 import { ClaimsIcon } from "@/src/assets/claims";
+import { MenuIcon } from "@/src/assets/menu-icon";
+import { useRef, useState } from "react";
+import { useClickAway } from "react-use";
 
 import styles from "./styles.module.css";
 
@@ -22,12 +25,64 @@ export function Nav() {
     const t = useTranslations("navigation");
     const pathname = usePathname();
 
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [menuWrapper, setMenuWrapper] = useState<HTMLDivElement | null>(null);
+    const menuPopoverRef = useRef<HTMLDivElement>(null);
+
+    useClickAway(menuPopoverRef, () => {
+        setMobileMenuOpen(false);
+    });
+
+    function handleMobileMenuOnClick() {
+        setMobileMenuOpen((prev) => !prev);
+    }
+
     return (
         <div className={classNames(styles.root)}>
             <div className={styles.main}>
-                <Link href="/">
-                    <MetromSquareLogo className={styles.metromLogo} />
-                </Link>
+                <div className={styles.leftContent}>
+                    <Link href="/">
+                        <MetromSquareLogo className={styles.metromLogo} />
+                    </Link>
+                    <div ref={menuPopoverRef}>
+                        <div
+                            ref={setMenuWrapper}
+                            className={classNames(styles.menuIconWrapper, {
+                                [styles.menuOpen]: mobileMenuOpen,
+                            })}
+                        >
+                            <MenuIcon onClick={handleMobileMenuOnClick} />
+                        </div>
+                        <Popover
+                            placement="bottom"
+                            anchor={menuWrapper}
+                            open={mobileMenuOpen}
+                        >
+                            <div className={styles.mobileNavigationWrapper}>
+                                {ROUTES.map(({ path, label, icon: Icon }) => (
+                                    <Link
+                                        key={path}
+                                        href={path}
+                                        onClick={handleMobileMenuOnClick}
+                                    >
+                                        <div
+                                            className={classNames(
+                                                styles.mobileNavLink,
+                                            )}
+                                        >
+                                            {Icon && (
+                                                <Icon
+                                                    className={styles.tabIcon}
+                                                />
+                                            )}
+                                            <Typography>{t(label)}</Typography>
+                                        </div>
+                                    </Link>
+                                ))}
+                            </div>
+                        </Popover>
+                    </div>
+                </div>
                 <div className={styles.chainStuffContainer}>
                     <ConnectButton />
                 </div>
