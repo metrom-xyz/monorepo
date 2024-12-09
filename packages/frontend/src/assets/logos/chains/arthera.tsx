0fb3a0d4eecdf13979ef82595c1013fc331d1903
luzzif <federico.luzzi@proton.me>
2024-12-09 10:35:40 +0000

    fix(frontend): fix arthera logo

--- packages/frontend/src/assets/logos/chains/arthera.tsx
+++ packages/frontend/src/assets/logos/chains/arthera.tsx
@@ -1,26 +1,90 @@
 export function ArtheraLogo(props: any) {
     return (
         <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            viewBox="0 0 202.7 177"
             xmlSpace="preserve"
+            width={183.2}
+            height={164.9}
+            x={0}
+            y={0}
+            version={1.1}
+            viewBox="0 0 183.2 164.9"
             {...props}
         >
-            <g>
-                <g>
-                    <g>
+            <defs />
+            <script type="text/javascript" />
+            <style type="text/css">
+                {
+                    ".st0{clip-path:url(#SVGID_00000114771027388254881450000008163069130819192479_);fill:url(#SVGID_00000062152681200052887040000008359632996752185984_);}"
+                }
+            </style>
+            <g
+                style={{
+                    fill: "#000",
+                    fillOpacity: 1,
+                }}
+                transform="translate(-9.1 -6.5)"
+            >
+                <g
+                    style={{
+                        fill: "#000",
+                        fillOpacity: 1,
+                    }}
+                >
+                    <g
+                        style={{
+                            fill: "#000",
+                            fillOpacity: 1,
+                        }}
+                    >
                         <defs>
                             <path
-                                id="SVGID_1_"
-                                d="M9.1,138.9v32.4h34.5v-32.4c0-15.3,12.5-27.8,27.8-27.8V76.6C37.1,76.6,9.1,104.6,9.1,138.9 M83.4,171.3 H118V40.9H83.4V171.3z M130,41.1c15.3,0,27.8,12.5,27.8,27.8v102.5h34.5V68.8c0-34.4-28-62.3-62.3-62.3V41.1z"
+                                id="a"
+                                d="m 9.1,138.9 v 32.4 h 34.5 v -32.4 c 0,-15.3 12.5,-27.8 27.8,-27.8 V 76.6 c -34.3,0 -62.3,28 -62.3,62.3 m 74.3,32.4 H 118 V 40.9 H 83.4 Z M 130,41.1 c 15.3,0 27.8,12.5 27.8,27.8 v 102.5 h 34.5 V 68.8 C 192.3,34.4 164.3,6.5 130,6.5 Z"
                             />
                         </defs>
-                        <clipPath id="SVGID_00000183250428108554251580000017932646614250471591_">
-                            <use xlinkHref="#SVGID_1_" overflow="visible" />
+                        <clipPath id="b">
+                            <use
+                                xlinkHref="#a"
+                                style={{
+                                    overflow: "visible",
+                                }}
+                            />
                         </clipPath>
-                        <polygon points="100.7,-85.1 274.7,88.9 100.7,262.9 -73.3,88.9" />
+                        <linearGradient
+                            x1={-746.298}
+                            x2={-745.298}
+                            y1={584.7}
+                            y2={584.7}
+                            gradientTransform="scale(164.508 -164.508) rotate(-45 -1079.148 -609.178)"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop
+                                offset={0}
+                                style={{
+                                    stopColor: "#1dc0cc",
+                                }}
+                            />
+                            <stop
+                                offset={0.5}
+                                style={{
+                                    stopColor: "#8e15f8",
+                                }}
+                            />
+                            <stop
+                                offset={1}
+                                style={{
+                                    stopColor: "#e20000",
+                                }}
+                            />
+                        </linearGradient>
+                        <polygon
+                            points="100.7,262.9 -73.3,88.9 100.7,-85.1 274.7,88.9"
+                            clipPath="url(#b)"
+                            style={{
+                                fill: "#000",
+                                fillOpacity: 1,
+                            }}
+                        />
                     </g>
                 </g>
             </g>
