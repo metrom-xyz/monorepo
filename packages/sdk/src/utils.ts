ae488299194efb99859af9a61e069e92648878c9
luzzif <federico.luzzi@proton.me>
2025-01-20 16:07:35 +0000

    chore(sdk): remove unscaled price to tick and tick to price function variants

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -1,13 +1,5 @@
 import type { AmmPool } from "./types/commons";
 
-export function priceToTick(price: number) {
-    return Math.round(Math.log(price) / Math.log(1.0001));
-}
-
-export function tickToPrice(tick: number) {
-    return Math.pow(1.0001, tick);
-}
-
 export function tickToScaledPrice(
     tick: number,
     pool: AmmPool,
@@ -17,7 +9,7 @@ export function tickToScaledPrice(
         ? pool.tokens[0].decimals - pool.tokens[1].decimals
         : pool.tokens[1].decimals - pool.tokens[0].decimals;
 
-    return tickToPrice(tick) * Math.pow(10, scalingFactor);
+    return Math.pow(1.0001, tick) * Math.pow(10, scalingFactor);
 }
 
 export function scaledPriceToTick(
@@ -29,5 +21,7 @@ export function scaledPriceToTick(
         ? pool.tokens[0].decimals - pool.tokens[1].decimals
         : pool.tokens[1].decimals - pool.tokens[0].decimals;
 
-    return priceToTick(price / Math.pow(10, scalingFactor));
+    const scaledPrice = price / Math.pow(10, scalingFactor);
+
+    return Math.round(Math.log(scaledPrice) / Math.log(1.0001));
 }
