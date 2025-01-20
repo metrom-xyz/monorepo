33d258aaf52c0fdb0033d375a6645dce7cbcdaf7
Paolo Guerra <pg.guerrap@proton.me>
2025-01-20 09:41:18 +0100

    feat(frontend): partial price flip implementation

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -8,14 +8,14 @@ export function tickToPrice(tick: number) {
     return Math.pow(1.0001, tick);
 }
 
-export function getPrice(tick: number, pool: AmmPool) {
+export function tickToScaledPrice(tick: number, pool: AmmPool) {
     return (
         tickToPrice(tick) *
         Math.pow(10, pool.tokens[0].decimals - pool.tokens[1].decimals)
     );
 }
 
-export function getTick(price: number, pool: AmmPool): number {
+export function priceToScaledTick(price: number, pool: AmmPool): number {
     return priceToTick(
         price / Math.pow(10, pool.tokens[0].decimals - pool.tokens[1].decimals),
     );
