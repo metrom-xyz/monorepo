carrot-kpi/metrom-monorepo

feat(frontend): add percentages to liquidity chart, add increment/decrement buttons

packages/frontend/src/components/create-campaign/preview/index.tsx
@@ -34,6 +34,7 @@ import {
 import { ENVIRONMENT } from "@/src/commons/env";
 import { Kpi } from "./kpi";
 import { AprChip } from "../../apr-chip";
+import { Range } from "./range";
 
 import styles from "./styles.module.css";
 
@@ -277,6 +278,12 @@ export function CampaignPreview({
                     onBack={onBack}
                 />
                 <div className={styles.content}>
+                    {!!payload.rangeSpecification && (
+                        <Range
+                            pool={payload.pool}
+                            specification={payload.rangeSpecification}
+                        />
+                    )}
                     {!!payload.kpiSpecification && (
                         <Kpi
                             poolUsdTvl={payload.pool?.usdTvl}

metrom-xyz/monorepo

packages/frontend/src/components/create-campaign/preview/index.tsx
@@ -35,6 +35,7 @@ import {
 import { ENVIRONMENT } from "@/src/commons/env";
 import { Kpi } from "./kpi";
 import { AprChip } from "../../apr-chip";
+import { Range } from "./range";
 
 import styles from "./styles.module.css";
 
@@ -276,6 +277,12 @@ export function CampaignPreview({
                     onBack={onBack}
                 />
                 <div className={styles.content}>
+                    {!!payload.priceRangeSpecification && (
+                        <Range
+                            pool={payload.pool}
+                            specification={payload.priceRangeSpecification}
+                        />
+                    )}
                     {!!payload.kpiSpecification && tokensCampaign && (
                         <Kpi
                             poolUsdTvl={payload.pool.usdTvl}