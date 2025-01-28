57e72ac944073f0d2b466c4fa87773dd9d7a494e
luzzif <federico.luzzi@proton.me>
2025-01-28 09:52:37 +0000

    fix(frontend): fix page/layout params typings

--- packages/frontend/src/app/[locale]/[...catch]/page.tsx
+++ packages/frontend/src/app/[locale]/[...catch]/page.tsx
@@ -2,11 +2,17 @@ import { routing } from "@/src/i18n/routing";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 
+interface Params {
+    locale: string;
+}
+
+interface NotFoundCatchAllProps {
+    params: Promise<Params>;
+}
+
 export default async function NotFoundCatchAll({
     params,
-}: {
-    params: { locale: string };
-}) {
+}: NotFoundCatchAllProps) {
     const { locale } = await params;
 
     if (!routing.locales.includes(locale as any)) {
