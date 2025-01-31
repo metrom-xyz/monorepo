cf4c0b9002fafff664e91d147a79cbee6c1c90e8
luzzif <federico.luzzi@proton.me>
2025-01-31 17:15:27 +0000

    feat(frontend): add ebisu support

--- packages/frontend/src/commons/chains/sepolia.ts
+++ packages/frontend/src/commons/chains/sepolia.ts
@@ -5,6 +5,7 @@ import { sepolia } from "viem/chains";
 import { SupportedLiquityV2, TargetType } from "@metrom-xyz/sdk";
 import { LiquityLogo } from "@/src/assets/logos/liquity-v2-platforms/liquity";
 import { ProtocolType } from "@/src/types";
+import { EbisuLogo } from "@/src/assets/logos/liquity-v2-platforms/ebisu";
 
 export const sepoliaData: ChainData = {
     name: sepolia.name,
@@ -24,6 +25,27 @@ export const sepoliaData: ChainData = {
                 symbol: "BOLD",
             },
             actionUrls: {
+                [TargetType.LiquityV2Debt]:
+                    "https://liquity2-sepolia.vercel.app/borrow",
+                [TargetType.LiquityV2Collateral]:
+                    "https://liquity2-sepolia.vercel.app/leverage",
+                [TargetType.LiquityV2StabilityPool]:
+                    "https://liquity2-sepolia.vercel.app/earn",
+            },
+        },
+        {
+            type: ProtocolType.LiquityV2,
+            slug: SupportedLiquityV2.Ebisu,
+            logo: EbisuLogo,
+            name: "Ebisu",
+            debtToken: {
+                address: "0xA4E721039da9e14332Fec14F98f463D92623149A",
+                decimals: 18,
+                name: "ebUSD",
+                symbol: "ebUSD",
+            },
+            actionUrls: {
+                // TODO: fix
                 [TargetType.LiquityV2Debt]:
                     "https://liquity2-sepolia.vercel.app/borrow",
                 [TargetType.LiquityV2Collateral]:
