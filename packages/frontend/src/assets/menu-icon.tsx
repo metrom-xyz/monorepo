24c31c3f5d483e736d0652732843b5a523b9ccb5
Paolo Guerra <pg.guerrap@proton.me>
2025-02-06 17:15:30 +0100

    fix(frontend): fix mobile menu

--- packages/frontend/src/assets/menu-icon.tsx
+++ packages/frontend/src/assets/menu-icon.tsx
@@ -8,9 +8,21 @@ export function MenuIcon(props: any) {
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
-            <rect x="4.5" y="7.14258" width="27" height="2" fill="black" />
-            <rect x="4.5" y="26.8574" width="27" height="2" fill="black" />
-            <rect x="4.5" y="17" width="27" height="2" fill="black" />
+            <rect
+                x="4.5"
+                y="7.14258"
+                width="27"
+                height="2"
+                fill="currentColor"
+            />
+            <rect
+                x="4.5"
+                y="26.8574"
+                width="27"
+                height="2"
+                fill="currentColor"
+            />
+            <rect x="4.5" y="17" width="27" height="2" fill="currentColor" />
         </svg>
     );
 }
