3f79c5a89b252856acc7b51d9bc5ca8c01beb32e
Paolo Guerra <pg.guerrap@proton.me>
2025-02-06 22:08:14 +0100

    fix(frontend): fix mobile nav

--- packages/frontend/src/components/layout/nav/index.tsx
+++ packages/frontend/src/components/layout/nav/index.tsx
@@ -4,14 +4,11 @@ import { Link, usePathname } from "@/src/i18n/routing";
 import { MetromSquareLogo } from "@/src/assets/metrom-square-logo";
 import { useTranslations } from "next-intl";
 import { ConnectButton } from "../../connect-button";
-import { Popover, Typography } from "@metrom-xyz/ui";
+import { Typography } from "@metrom-xyz/ui";
 import classNames from "classnames";
 import { NewCampaignIcon } from "@/src/assets/new-campaign-icon";
 import { AllCampaignsIcon } from "@/src/assets/all-campaigns-icon";
 import { ClaimsIcon } from "@/src/assets/claims";
-import { MenuIcon } from "@/src/assets/menu-icon";
-import { useRef, useState } from "react";
-import { useClickAway } from "react-use";
 import { ThemeSwitcher } from "../../theme-switcher";
 
 import styles from "./styles.module.css";
@@ -26,18 +23,6 @@ export function Nav() {
     const t = useTranslations("navigation");
     const pathname = usePathname();
 
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-    const [menuWrapper, setMenuWrapper] = useState<HTMLDivElement | null>(null);
-    const menuPopoverRef = useRef<HTMLDivElement>(null);
-
-    useClickAway(menuPopoverRef, () => {
-        setMobileMenuOpen(false);
-    });
-
-    function handleMobileMenuOnClick() {
-        setMobileMenuOpen((prev) => !prev);
-    }
-
     return (
         <div className={classNames(styles.root)}>
             <div className={styles.main}>
@@ -45,44 +30,6 @@ export function Nav() {
                     <Link href="/">
                         <MetromSquareLogo className={styles.metromLogo} />
                     </Link>
-                    <div ref={menuPopoverRef}>
-                        <div
-                            ref={setMenuWrapper}
-                            className={classNames(styles.menuIconWrapper)}
-                        >
-                            <MenuIcon onClick={handleMobileMenuOnClick} />
-                        </div>
-                        <Popover
-                            placement="bottom"
-                            anchor={menuWrapper}
-                            open={mobileMenuOpen}
-                        >
-                            <div className={styles.mobileNavigationWrapper}>
-                                {ROUTES.map(({ path, label, icon: Icon }) => (
-                                    <Link
-                                        key={path}
-                                        href={path}
-                                        onClick={handleMobileMenuOnClick}
-                                    >
-                                        <div
-                                            className={classNames(
-                                                styles.mobileNavLink,
-                                            )}
-                                        >
-                                            {Icon && (
-                                                <Icon
-                                                    className={styles.tabIcon}
-                                                />
-                                            )}
-                                            <Typography weight="medium">
-                                                {t(label)}
-                                            </Typography>
-                                        </div>
-                                    </Link>
-                                ))}
-                            </div>
-                        </Popover>
-                    </div>
                 </div>
                 <div className={styles.rightContentContainer}>
                     <ThemeSwitcher />
@@ -104,6 +51,22 @@ export function Nav() {
                                     {t(label)}
                                 </Typography>
                             </div>
+                        </Link>
+                    ))}
+                </div>
+                <div className={styles.mobileNavigationWrapper}>
+                    {ROUTES.map(({ path, label, icon: Icon }) => (
+                        <Link
+                            key={path}
+                            href={path}
+                            className={classNames(styles.mobileNavLink, {
+                                [styles.tabActive]:
+                                    pathname === path ||
+                                    (path !== "/" && pathname.startsWith(path)),
+                            })}
+                        >
+                            {Icon && <Icon className={styles.tabIcon} />}
+                            <Typography weight="medium">{t(label)}</Typography>
                         </Link>
                     ))}
                 </div>
