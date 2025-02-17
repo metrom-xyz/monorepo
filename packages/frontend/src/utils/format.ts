c8523cbd459f5b5ff8521451204a79a60815587f
Paolo Guerra <pg.guerrap@proton.me>
2025-02-17 14:57:58 +0100

    fix(frontend): fix percentages and amounts formatting

--- packages/frontend/src/utils/format.ts
+++ packages/frontend/src/utils/format.ts
@@ -1,40 +1,33 @@
 import dayjs, { Dayjs } from "dayjs";
 import numeral from "numeral";
 
-const HUMANIZE_CUTOFF = 100000;
+const HUMANIZE_AMOUNT_CUTOFF = 100000;
+const HUMANIZE_PERCENTAGE_CUTOFF = 10000;
 
 export function formatUsdAmount(amount?: number | null): string {
     if (amount && amount < 0.01) return "$<0.01";
     return numeral(amount).format(
-        `($0,0.0[0]${amount && amount > HUMANIZE_CUTOFF ? "a" : ""})`,
+        `($0,0.0[0]${amount && amount > HUMANIZE_AMOUNT_CUTOFF ? "a" : ""})`,
     );
 }
 
 export function formatPercentage({
     percentage,
     keepDust,
-    humanize = true,
 }: {
     percentage?: number | null;
     keepDust?: boolean;
-    humanize?: boolean;
 }): string {
     if (percentage && !keepDust && percentage < 0.01) return "<0.01%";
-    if (percentage && percentage > 10000) return ">10k%";
-    return `${numeral(percentage).format(`0,0.0[${keepDust ? "000" : "0"}]${humanize ? "a" : ""}`)}%`;
+    if (percentage && percentage > HUMANIZE_PERCENTAGE_CUTOFF) return ">10k%";
+    return `${numeral(percentage).format(`0,0.0[${keepDust ? "000" : "0"}]`)}%`;
 }
 
-export function formatAmount({
-    amount,
-    humanize = true,
-}: {
-    amount?: number | null;
-    humanize?: boolean;
-}): string {
+export function formatAmount({ amount }: { amount?: number | null }): string {
     if (amount && amount < 0.0001) return "<0.0001";
-    if (amount && amount > HUMANIZE_CUTOFF)
-        return numeral(amount).format(`0,0.0[000]`);
-    return numeral(amount).format(`0,0.0[000]${humanize ? "a" : ""}`);
+    return numeral(amount).format(
+        `0,0.0[000]${amount && amount > HUMANIZE_AMOUNT_CUTOFF ? "a" : ""}`,
+    );
 }
 
 export function formatDateTime(dateTime?: Dayjs | number): string {
