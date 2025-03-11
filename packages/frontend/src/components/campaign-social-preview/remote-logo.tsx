0c347b18e75bcf9abf9d7037dce775d0f6da3603
Paolo Guerra <pg.guerrap@proton.me>
2025-03-11 18:09:59 +0100

    feat(frontend): add campaign details social link preview

---
+++ packages/frontend/src/components/campaign-social-preview/remote-logo.tsx
@@ -0,0 +1,32 @@
+import type { CSSProperties } from "react";
+
+interface RemoteLogoProps {
+    src?: string;
+    defaultText?: string;
+    style?: CSSProperties | undefined;
+}
+
+export function RemoteLogo({ src, defaultText = "?", style }: RemoteLogoProps) {
+    if (src) {
+        return (
+            <div tw="flex relative h-14 w-14" style={style}>
+                <img src={src} tw="absolute" />
+            </div>
+        );
+    }
+
+    return (
+        <div
+            tw="flex relative items-center justify-center rounded-full bg-[#27272a] h-14 w-14"
+            style={style}
+        >
+            <span tw="text-white">
+                {defaultText
+                    ? defaultText.length > 4
+                        ? `${defaultText.slice(0, 4)}`
+                        : defaultText
+                    : "?"}
+            </span>
+        </div>
+    );
+}
