18bf5ff38f6b6e5e91de54790de1164f18c34752
Paolo Guerra <pg.guerrap@proton.me>
2025-03-12 15:37:07 +0100

    feat(frontend): add neutral opengraph image to routes

--- packages/frontend/src/app/[locale]/claims/page.tsx
+++ packages/frontend/src/app/[locale]/claims/page.tsx
@@ -1,5 +1,11 @@
 import { Claims } from "@/src/components/claims";
 
+export const metadata = {
+    openGraph: {
+        images: ["/images/opengraph-image.png"],
+    },
+};
+
 export default function ClaimsPage() {
     return <Claims />;
 }
