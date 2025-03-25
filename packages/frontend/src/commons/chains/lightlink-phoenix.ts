c9a1260848c5e8579088750d7c56db16ded56eb0
luzzif <federico.luzzi@proton.me>
2025-03-25 09:34:04 +0000

    chore(frontend): add base tokens to lightlink

--- packages/frontend/src/commons/chains/lightlink-phoenix.ts
+++ packages/frontend/src/commons/chains/lightlink-phoenix.ts
@@ -32,5 +32,23 @@ export const lightlinkPhoenixData: ChainData = {
             name: "Wrapped Ether",
             symbol: "WETH",
         },
+        {
+            address: "0xd9d7123552fA2bEdB2348bB562576D67f6E8e96E",
+            decimals: 18,
+            name: "LightLink",
+            symbol: "LL.e",
+        },
+        {
+            address: "0x808d7c71ad2ba3FA531b068a2417C63106BC0949",
+            decimals: 6,
+            name: "Tether USD (Stargate Bridged)",
+            symbol: "USDT",
+        },
+        {
+            address: "0xbCF8C1B03bBDDA88D579330BDF236B58F8bb2cFd",
+            decimals: 6,
+            name: "Bridged USDC (Stargate)",
+            symbol: "USDC.e",
+        },
     ],
 };
