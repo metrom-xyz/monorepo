d8e06b3d6af191dc2ff9ed8520f67e17a63c1bcf
Paolo Guerra <pg.guerrap@proton.me>
2025-04-18 15:00:52 +0200

    fix(frontend): keep connected account rank if leaderboard has 5 ranks

--- packages/frontend/src/hooks/useLeaderboard.ts
+++ packages/frontend/src/hooks/useLeaderboard.ts
@@ -88,7 +88,7 @@ export function useLeaderboard({
 
                 const connectedAccountRank =
                     connectedAccountRankIndex !== -1
-                        ? sortedRanks.length < 5
+                        ? sortedRanks.length <= 5
                             ? sortedRanks[connectedAccountRankIndex]
                             : sortedRanks.splice(
                                   connectedAccountRankIndex,
