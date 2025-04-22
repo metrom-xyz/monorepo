807d435221d69905fcaf78550529ce974f936c42
Paolo Guerra <pg.guerrap@proton.me>
2025-04-22 17:49:03 +0200

    feat(frontend/ui): add lv2 points campaigns banner, minor fixes to details page

--- packages/frontend/src/components/lv2-points-campaign/header/index.tsx
+++ packages/frontend/src/components/lv2-points-campaign/header/index.tsx
@@ -2,24 +2,37 @@ import type { SVGIcon } from "@/src/types/common";
 import type { FunctionComponent } from "react";
 import { Button, Typography } from "@metrom-xyz/ui";
 import { ArrowRightIcon } from "@/src/assets/arrow-right-icon";
+import { useTranslations } from "next-intl";
+import type { BrandColor } from "@/src/types/lv2-points-campaign";
 
 import styles from "./styles.module.css";
-import { useTranslations } from "next-intl";
 
 interface HeaderProps {
     name: string;
     description: string;
+    brand: BrandColor;
     url: string;
     icon: FunctionComponent<SVGIcon>;
 }
 
-export function Header({ name, description, url, icon: Icon }: HeaderProps) {
+export function Header({
+    name,
+    description,
+    brand,
+    url,
+    icon: Icon,
+}: HeaderProps) {
     const t = useTranslations("lv2PointsCampaignPage.header");
 
     return (
         <div className={styles.root}>
             <div className={styles.leftContent}>
-                <Icon />
+                <div
+                    className={styles.iconWrapper}
+                    style={{ backgroundColor: brand.light }}
+                >
+                    <Icon className={styles.icon} />
+                </div>
                 <div className={styles.titleWrapper}>
                     <Typography weight="medium" size="xl4" uppercase>
                         {t("title", { protocol: name })}
