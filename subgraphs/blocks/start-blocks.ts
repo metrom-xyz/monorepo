37dddc9fdb9ae6d879f7313f1f22510507d7bc8b
luzzif <federico.luzzi@proton.me>
2025-04-30 13:29:39 +0100

    chore(blocks-subgraph): deploy new version

--- subgraphs/blocks/start-blocks.ts
+++ subgraphs/blocks/start-blocks.ts
@@ -1,5 +1,5 @@
 export const START_BLOCKS: { [network: string]: number } = {
-    holesky: 3755153,
+    holesky: 3760157,
     "arbitrum-one": 154000000,
     "base-sepolia": 16132650,
     sepolia: 7483369,
