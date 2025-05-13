e13e5cf92d28d3bab599223103382c76ce8f7333
Paolo Guerra <pg.guerrap@proton.me>
2025-05-13 14:30:38 +0200

    fix(frontend): add mock lv2 dev configuration

--- packages/frontend/src/commons/lv2-points/lv2-dev.ts
+++ packages/frontend/src/commons/lv2-points/lv2-dev.ts
@@ -1,11 +1,141 @@
-import type { Lv2PointsCampaign } from "@/src/types/lv2-points-campaign";
+import { QuillLogo } from "@/src/assets/logos/liquity-v2-platforms/quill";
+import {
+    Lv2PointsCampaign2Action,
+    type Lv2PointsCampaign,
+} from "@/src/types/lv2-points-campaign";
+import { SupportedChain } from "@metrom-xyz/contracts";
 import { SupportedLiquityV2 } from "@metrom-xyz/sdk";
 
 export const lv2PointsCampaignsDev: Record<
     SupportedLiquityV2,
     Lv2PointsCampaign | null
 > = {
-    [SupportedLiquityV2.Quill]: null,
+    [SupportedLiquityV2.Quill]: {
+        name: "Quill finance",
+        description:
+            "A secure, over-collateralized stablecoin protocol on Scroll's zk-Rollup network.",
+        url: "https://app.quill.finance",
+        chain: SupportedChain.Scroll,
+        protocol: SupportedLiquityV2.Quill,
+        pointsName: "Quill Liquidity Points",
+        brand: { main: "#FF5500", light: "#FFBFA3" },
+        icon: QuillLogo,
+        protocolIntro: {
+            articles: [
+                {
+                    title: "How Quill and Scroll Are Redefining DeFi Together",
+                    href: "https://medium.com/@QuillFi/how-quill-and-scroll-are-redefining-defi-together-47aeba67d995",
+                },
+                {
+                    title: "Fueling DeFi: Quill's Revamped Incentives Programs",
+                    href: "https://medium.com/@QuillFi/fueling-defi-quills-revamped-incentives-programs-926e4ffd9034",
+                },
+            ],
+        },
+        from: 1746720000,
+        to: 1749398400,
+        actions: {
+            [Lv2PointsCampaign2Action.Liquidity]: {
+                title: "Liquidity",
+                description:
+                    "Users get incentivized to provide liquidity in USDQ/USDC in AMMs.",
+                actions: [
+                    {
+                        targets: [
+                            "0xdb9E8F82D6d45fFf803161F2a5f75543972B229a",
+                            "0x06efdbff2a14a7c8e15944d1f4a48f9f95f663a4",
+                        ],
+                        minimumDuration: 3600,
+                        name: "USDQ/USDC LP",
+                        description: "Ambient",
+                        multiplier: 2,
+                        href: "https://ambient.finance/trade/pool/chain=0x82750&tokenA=0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4&tokenB=0xdb9e8f82d6d45fff803161f2a5f75543972b229a",
+                    },
+                    {
+                        targets: [
+                            "0xdb9E8F82D6d45fFf803161F2a5f75543972B229a",
+                            "0x06efdbff2a14a7c8e15944d1f4a48f9f95f663a4",
+                        ],
+                        minimumDuration: 3600,
+                        name: "USDQ/USDC LP",
+                        description: "Honeypop",
+                        multiplier: 2,
+                        href: "https://honeypop.app/add/0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4/0xdb9E8F82D6d45fFf803161F2a5f75543972B229a/500",
+                    },
+                ],
+            },
+            [Lv2PointsCampaign2Action.StabilityPool]: {
+                title: "Deposit to stability pool",
+                description:
+                    "Users get incentivized to deposit liquidity in stability pools.",
+                actions: [
+                    {
+                        targets: ["0x5300000000000000000000000000000000000004"],
+                        minimumDuration: 3600,
+                        name: "ETH stability pool",
+                        multiplier: 3,
+                        href: "https://app.quill.finance/earn/eth",
+                    },
+                    {
+                        targets: ["0xf610A9dfB7C89644979b4A0f27063E9e7d7Cda32"],
+                        minimumDuration: 3600,
+                        name: "stETH stability pool",
+                        multiplier: 3,
+                        href: "https://app.quill.finance/earn/wsteth",
+                    },
+                    {
+                        targets: ["0x01f0a31698C4d065659b9bdC21B3610292a1c506"],
+                        minimumDuration: 3600,
+                        name: "weETH stability pool",
+                        multiplier: 2,
+                        href: "https://app.quill.finance/earn/weeth",
+                    },
+                    {
+                        targets: ["0xd29687c813D741E2F938F4aC377128810E217b1b"],
+                        minimumDuration: 3600,
+                        name: "SCR stability pool",
+                        multiplier: 5,
+                        href: "https://app.quill.finance/earn/scr",
+                    },
+                ],
+            },
+            [Lv2PointsCampaign2Action.Debt]: {
+                title: "Borrow",
+                description:
+                    "Users get incentivized to deposit collateral and mint USDQ.",
+                actions: [
+                    {
+                        targets: ["0x5300000000000000000000000000000000000004"],
+                        minimumDuration: 3600,
+                        name: "Borrow using ETH",
+                        multiplier: 3,
+                        href: "https://app.quill.finance/borrow/eth",
+                    },
+                    {
+                        targets: ["0xf610A9dfB7C89644979b4A0f27063E9e7d7Cda32"],
+                        minimumDuration: 3600,
+                        name: "Borrow using stETH",
+                        multiplier: 3,
+                        href: "https://app.quill.finance/borrow/wsteth",
+                    },
+                    {
+                        targets: ["0x01f0a31698C4d065659b9bdC21B3610292a1c506"],
+                        minimumDuration: 3600,
+                        name: "Borrow using weETH",
+                        multiplier: 2,
+                        href: "https://app.quill.finance/borrow/weeth",
+                    },
+                    {
+                        targets: ["0xd29687c813D741E2F938F4aC377128810E217b1b"],
+                        minimumDuration: 3600,
+                        name: "Borrow using SCR",
+                        multiplier: 5,
+                        href: "https://app.quill.finance/borrow/scr",
+                    },
+                ],
+            },
+        },
+    },
     [SupportedLiquityV2.Orki]: null,
     [SupportedLiquityV2.Ebisu]: null,
     [SupportedLiquityV2.Liquity]: null,
