c49ec7b4c3f6d2bf81302cc32e08afef460219c9
Paolo Guerra <pg.guerrap@proton.me>
2025-05-19 14:51:02 +0200

    feat(frontend): skip campaign type pick if only one type is supported

--- packages/frontend/src/components/create-campaign/index.tsx
+++ packages/frontend/src/components/create-campaign/index.tsx
@@ -1,36 +1,75 @@
 "use client";
 
 import { Card, Typography } from "@metrom-xyz/ui";
-import { Link } from "@/src/i18n/routing";
-import { useTranslations } from "next-intl";
+import { Link, redirect } from "@/src/i18n/routing";
+import { useLocale, useTranslations } from "next-intl";
 import { LiquityV2CampaignIcon } from "@/src/assets/liquity-v2-campaign-icon";
 import { AmmCampaignIcon } from "@/src/assets/amm-campaign-icon";
 import classNames from "classnames";
 import { CampaignType } from "@/src/types/campaign";
+import { useProtocolsInChain } from "@/src/hooks/useProtocolsInChain";
+import { useChainId } from "wagmi";
+import { ProtocolType } from "@/src/types/protocol";
+import { useMemo } from "react";
+import { RedirectType } from "next/navigation";
 
 import styles from "./styles.module.css";
 
-const CAMPAIGN_TYPES = [
-    {
-        enabled: true,
-        path: `/campaigns/create/${CampaignType.AmmPoolLiquidity}`,
-        title: "amm.title",
-        description: "amm.description",
-        className: styles.ammFormIcon,
-        icon: <AmmCampaignIcon className={styles.ammIcon} />,
-    },
-    {
-        enabled: true,
-        path: `/campaigns/create/${CampaignType.LiquityV2}`,
-        title: "liquityV2.title",
-        description: "liquityV2.description",
-        className: styles.liquityV2FormIcon,
-        icon: <LiquityV2CampaignIcon className={styles.liquidityV2Icon} />,
-    },
-];
-
 export function CreateCampaign() {
     const t = useTranslations("newCampaign.pickType");
+    const locale = useLocale();
+
+    const chainId = useChainId();
+    const dexesProtocols = useProtocolsInChain(chainId, ProtocolType.Dex);
+    const liquityV2Protocols = useProtocolsInChain(
+        chainId,
+        ProtocolType.LiquityV2,
+    );
+
+    const campaignTypes = useMemo(() => {
+        return [
+            {
+                enabled: dexesProtocols.length > 0,
+                path: `/campaigns/create/${CampaignType.AmmPoolLiquidity}`,
+                title: "amm.title",
+                description: "amm.description",
+                className: styles.ammFormIcon,
+                icon: <AmmCampaignIcon className={styles.ammIcon} />,
+            },
+            {
+                enabled: liquityV2Protocols.length > 0,
+                path: `/campaigns/create/${CampaignType.LiquityV2}`,
+                title: "liquityV2.title",
+                description: "liquityV2.description",
+                className: styles.liquityV2FormIcon,
+                icon: (
+                    <LiquityV2CampaignIcon className={styles.liquidityV2Icon} />
+                ),
+            },
+        ];
+    }, [dexesProtocols.length, liquityV2Protocols.length]);
+
+    if (dexesProtocols.length === 0)
+        redirect(
+            {
+                href: {
+                    pathname: `/campaigns/create/${CampaignType.LiquityV2}`,
+                },
+                locale,
+            },
+            RedirectType.replace,
+        );
+
+    if (liquityV2Protocols.length === 0)
+        redirect(
+            {
+                href: {
+                    pathname: `/campaigns/create/${CampaignType.AmmPoolLiquidity}`,
+                },
+                locale,
+            },
+            RedirectType.replace,
+        );
 
     return (
         <div className={styles.root}>
@@ -41,8 +80,9 @@ export function CreateCampaign() {
                 {t("description")}
             </Typography>
             <div className={styles.campaignCardsWrapper}>
-                {CAMPAIGN_TYPES.filter(({ enabled }) => enabled).map(
-                    ({ path, title, description, icon, className }) => (
+                {campaignTypes
+                    .filter(({ enabled }) => enabled)
+                    .map(({ path, title, description, icon, className }) => (
                         <Link key={path} href={path}>
                             <Card className={styles.campaignCard}>
                                 <div className={styles.campaignCardBody}>
@@ -65,8 +105,7 @@ export function CreateCampaign() {
                                 </div>
                             </Card>
                         </Link>
-                    ),
-                )}
+                    ))}
             </div>
         </div>
     );
