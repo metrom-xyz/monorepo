0666b1ca1e6388f567316ef315b1ffde91bcc9da
Paolo Guerra <pg.guerrap@proton.me>
2025-05-22 16:30:18 +0200

    fix: outdated lock file

--- packages/chains-data/src/assets/logos/dexes/fibonacci.tsx
+++ packages/chains-data/src/assets/logos/dexes/fibonacci.tsx
@@ -1,5 +1,6 @@
 import fibonacciLogo from "./fibonacci.webp";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function FibonacciLogo(props: any) {
     return <img {...props} src={fibonacciLogo} alt="Fibonacci logo" />;
 }
