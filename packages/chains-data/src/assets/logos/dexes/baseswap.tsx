ebe3968afbfc54deaf17cace84ddaeed6c32d36a
Paolo Guerra <pg.guerrap@proton.me>
2025-05-22 16:30:18 +0200

    fix: outdated lock file

--- packages/chains-data/src/assets/logos/dexes/baseswap.tsx
+++ packages/chains-data/src/assets/logos/dexes/baseswap.tsx
@@ -1,5 +1,6 @@
 import baseSwapLogo from "./baseswap.webp";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function BaseSwapLogo(props: any) {
     return <img {...props} src={baseSwapLogo} alt="BaseSwap logo" />;
 }
