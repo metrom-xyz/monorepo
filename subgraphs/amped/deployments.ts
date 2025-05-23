5333ecdc13de43eebb32c0addf7fdaffec8981e5
luzzif <federico.luzzi@proton.me>
2025-05-23 09:26:09 +0100

    chore(amped-subgraph): add ll deployment

--- subgraphs/amped/deployments.ts
+++ subgraphs/amped/deployments.ts
@@ -11,6 +11,16 @@ interface ChainConfig {
 export const DEPLOYMENTS: {
     [network: string]: ChainConfig;
 } = {
+    lightlink: {
+        Vault: {
+            address: "0xa6b88069EDC7a0C2F062226743C8985FF72bB2Eb",
+            startBlock: 85247813,
+        },
+        GlpManager: {
+            address: "0xEF301eD9eAFf7476605aE8C8A7b966c0fbE77530",
+            startBlock: 85247896,
+        },
+    },
     sonic: {
         Vault: {
             address: "0x5B8caae7cC6Ea61fb96Fd251C4Bc13e48749C7Da",
