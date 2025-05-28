fe2d11a1f8aae8df5b0ff3368a6ce3bd03838492
Paolo Guerra <pg.guerrap@proton.me>
2025-05-28 15:26:41 +0200

    fix(frontend): fix env condition

--- packages/frontend/src/commons/env.ts
+++ packages/frontend/src/commons/env.ts
@@ -11,7 +11,7 @@ export const SAFE: boolean = process.env.NEXT_PUBLIC_SAFE === "true";
 
 export const WALLETCONNECT_PROJECT_ID: string =
     process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
-if (SAFE && !WALLETCONNECT_PROJECT_ID)
+if (!SAFE && !WALLETCONNECT_PROJECT_ID)
     throw new Error(
         "A valid NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID env variable is needed",
     );
