0ba9f6a2ca9b0e1a64f4980630134c411d50af7d
Paolo Guerra <pg.guerrap@proton.me>
2025-06-16 18:29:14 +0200

    fix(frontend): add use client to turtle details page

--- packages/frontend/src/app/[locale]/turtle-campaigns/[campaignId]/page.tsx
+++ packages/frontend/src/app/[locale]/turtle-campaigns/[campaignId]/page.tsx
@@ -1,9 +1,8 @@
-import { TURTLE_CAMPAIGNS } from "@/src/commons/turtle-campaigns";
+"use client";
+
 import { TurtleCampaignDetails } from "@/src/components/turtle-campaign-details";
 import { routing, type Locale } from "@/src/i18n/routing";
-import { getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
-import { title } from "process";
 
 export interface Params {
     campaignId: string;
@@ -14,19 +13,6 @@ export interface TurtleDealDetailsPageProps {
     params: Promise<Params>;
 }
 
-export async function generateMetadata({ params }: TurtleDealDetailsPageProps) {
-    const { campaignId } = await params;
-    const t = await getTranslations();
-
-    const campaign = TURTLE_CAMPAIGNS.find(({ id }) => id === campaignId);
-
-    return {
-        title: campaign
-            ? t("turtleCampaignPage.header.title", { owner: campaign.owner })
-            : t("turtleCampaignPage.header.fallback"),
-    };
-}
-
 export default async function TurtleCampaignDetailsPage({
     params,
 }: TurtleDealDetailsPageProps) {
@@ -34,11 +20,5 @@ export default async function TurtleCampaignDetailsPage({
 
     if (!routing.locales.includes(locale)) notFound();
 
-    setRequestLocale(locale);
-
     return <TurtleCampaignDetails campaignId={campaignId} />;
-}
-
-export function generateStaticParams() {
-    return routing.locales.map((locale) => ({ locale }));
 }
--- packages/frontend/src/commons/turtle-campaigns/index.ts
+++ packages/frontend/src/commons/turtle-campaigns/index.ts
@@ -1,6 +1,7 @@
 import type { TurtleCampaign } from "@/src/types/turtle";
 import { KatanaLogo } from "@metrom-xyz/chains";
 
+// TODO: enable per single env?
 export const TURTLE_CAMPAIGNS: TurtleCampaign[] = [
     {
         id: "2c86d3a1-cfe8-486d-915d-1b9ff5e924e9",
