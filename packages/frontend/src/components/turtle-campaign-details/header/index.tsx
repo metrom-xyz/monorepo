f94f741a235c5df62f2261634979d5a85fb2c342
Paolo Guerra <pg.guerrap@proton.me>
2025-06-17 15:11:03 +0200

    feat(frontend): add turtle distributor link, improve earn page styles

--- packages/frontend/src/components/turtle-campaign-details/header/index.tsx
+++ packages/frontend/src/components/turtle-campaign-details/header/index.tsx
@@ -9,57 +9,71 @@ import styles from "./styles.module.css";
 
 interface HeaderProps {
     name: string;
+    distributor: string;
     owner: string;
     description: string;
     brand: BrandColor;
     url: string;
-    icon: FunctionComponent<SVGIcon>;
+    ownerLogo: FunctionComponent<SVGIcon>;
 }
 
 export function Header({
     name,
+    distributor,
     owner,
     description,
     brand,
     url,
-    icon: Icon,
+    ownerLogo: Icon,
 }: HeaderProps) {
     const t = useTranslations("turtleCampaignPage.header");
 
     return (
         <div className={styles.root}>
-            <div className={styles.leftContent}>
-                <div
-                    className={styles.iconWrapper}
-                    style={{ backgroundColor: brand.light }}
+            <div className={styles.titleContainer}>
+                <div className={styles.leftContent}>
+                    <div
+                        className={styles.iconWrapper}
+                        style={{ backgroundColor: brand.light }}
+                    >
+                        <Icon className={styles.icon} />
+                    </div>
+                    <div className={styles.titleWrapper}>
+                        <Typography weight="medium" size="xl4" uppercase>
+                            {t("title", { owner })}
+                        </Typography>
+                        <Typography weight="medium" light size="sm">
+                            {description}
+                        </Typography>
+                    </div>
+                </div>
+                <Button
+                    size="sm"
+                    href={url}
+                    border={false}
+                    variant="secondary"
+                    icon={ArrowRightIcon}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={{
+                        root: styles.exploreButton,
+                        contentWrapper: styles.exploreButton,
+                        icon: styles.externalLinkIcon,
+                    }}
                 >
-                    <Icon className={styles.icon} />
-                </div>
-                <div className={styles.titleWrapper}>
-                    <Typography weight="medium" size="xl4" uppercase>
-                        {t("title", { owner })}
-                    </Typography>
-                    <Typography weight="medium" light size="sm">
-                        {description}
-                    </Typography>
-                </div>
+                    {name}
+                </Button>
             </div>
-            <Button
-                size="sm"
-                href={url}
-                border={false}
-                variant="secondary"
-                icon={ArrowRightIcon}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={{
-                    root: styles.exploreButton,
-                    contentWrapper: styles.exploreButton,
-                    icon: styles.externalLinkIcon,
-                }}
-            >
-                {name}
-            </Button>
+            <div className={styles.actionsContainer}>
+                <Button
+                    size="sm"
+                    href={distributor}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {t("deposit")}
+                </Button>
+            </div>
         </div>
     );
 }
