3b5ae806caa3c9405394694aad137dc71378c1e6
Paolo Guerra <pg.guerrap@proton.me>
2025-06-24 18:59:29 +0200

    fix(frontend): add suspense for create campaign page, minor fixes

--- packages/frontend/src/components/create-campaign/steps/end-date-step/index.tsx
+++ packages/frontend/src/components/create-campaign/steps/end-date-step/index.tsx
@@ -98,7 +98,7 @@ export function EndDateStep({
     useEffect(() => {
         if (disabled || !!endDate) return;
         setOpen(true);
-    }, [disabled]);
+    }, [disabled, endDate]);
 
     useEffect(() => {
         if (!endDate) setDate(undefined);
