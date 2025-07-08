57720d3dca771450dd468f4ef77b4edba1a41620
Paolo Guerra <pg.guerrap@proton.me>
2025-07-08 16:39:39 +0200

    fix(frontend): jump directly to closest date

--- packages/frontend/src/utils/date.ts
+++ packages/frontend/src/utils/date.ts
@@ -1,9 +1,12 @@
 import dayjs, { Dayjs } from "dayjs";
 
 export const getClosestAvailableDateTime = (date?: Dayjs | null) => {
-    const minutes = dayjs(date).get("minutes");
-    if (minutes < 15) return dayjs(date).set("minutes", 15).set("seconds", 0);
-    if (minutes < 30) return dayjs(date).set("minutes", 30).set("seconds", 0);
-    if (minutes < 45) return dayjs(date).set("minutes", 45).set("seconds", 0);
-    return dayjs(date).add(1, "hour").set("minutes", 0).set("seconds", 0);
+    let base = date;
+    if (date?.isBefore(dayjs())) base = dayjs();
+
+    const minutes = dayjs(base).get("minutes");
+    if (minutes < 15) return dayjs(base).set("minutes", 15).set("seconds", 0);
+    if (minutes < 30) return dayjs(base).set("minutes", 30).set("seconds", 0);
+    if (minutes < 45) return dayjs(base).set("minutes", 45).set("seconds", 0);
+    return dayjs(base).add(1, "hour").set("minutes", 0).set("seconds", 0);
 };
