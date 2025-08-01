1b543ff172c5b2a4e5e14680b3e44a2af8c2a6d9
Paolo Guerra <pg.guerrap@proton.me>
2025-08-01 17:03:33 +0200

    fix(frontend): add middleware for csp header

--- packages/frontend/src/middleware.ts
+++ packages/frontend/src/middleware.ts
@@ -1,8 +1,66 @@
+import {
+    createNEMO,
+    type GlobalMiddlewareConfig,
+    type MiddlewareConfig,
+} from "@rescale/nemo";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+const middlewares = {
+    "/": [
+        async (request) => {
+            return intlMiddleware(request);
+        },
+    ],
+} satisfies MiddlewareConfig;
+
+const globalMiddlewares = {
+    before: async (request) => {
+        const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+        const cspHeader = `
+                default-src 'self';
+                connect-src 'self' https:;
+                script-src 'self' https://cdn.usefathom.com 'nonce-${nonce}' 'strict-dynamic';
+                style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+                img-src * data: blob:;
+                font-src 'self' https://fonts.gstatic.com;
+                object-src 'none';
+                base-uri 'self';
+                form-action 'self';
+                frame-ancestors 'none';
+                upgrade-insecure-requests;
+            `;
+        const contentSecurityPolicyHeaderValue = cspHeader
+            .replace(/\s{2,}/g, " ")
+            .trim();
+
+        const requestHeaders = new Headers(request.headers);
+        requestHeaders.set("x-nonce", nonce);
+        requestHeaders.set(
+            "Content-Security-Policy",
+            contentSecurityPolicyHeaderValue,
+        );
+
+        const response = NextResponse.next({
+            request: {
+                headers: requestHeaders,
+            },
+        });
+        response.headers.set(
+            "Content-Security-Policy",
+            contentSecurityPolicyHeaderValue,
+        );
+        response.headers.set("X-Frame-Options", "DENY");
+
+        return response;
+    },
+} satisfies GlobalMiddlewareConfig;
 
 export const config = {
     matcher: ["/((?!api|_next|.*\\..*).*)", "/(en)/:path*"],
 };
+
+export default createNEMO(middlewares, globalMiddlewares);
