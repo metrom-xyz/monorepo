1a3ed6f13bf150ffddf13132267175afdd9d8226
Paolo Guerra <pg.guerrap@proton.me>
2025-08-13 18:42:22 +0200

    fix(frontend): remove duplicated aptos js pro client

--- packages/frontend/src/components/aptos-core-provider.tsx
+++ packages/frontend/src/components/aptos-core-provider.tsx
@@ -2,19 +2,14 @@ import type { PropsWithChildren } from "react";
 import { AptosJSCoreProvider } from "@aptos-labs/react";
 import { useWalletAdapterCore } from "@aptos-labs/react/connectors";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
-import { type NetworkInfo } from "@aptos-labs/js-pro";
-import { ENVIRONMENT } from "../commons/env";
-import { Environment } from "@metrom-xyz/sdk";
-import { Network } from "@aptos-labs/ts-sdk";
+import { APTOS_CLIENT_API_KEY } from "../commons/env";
+import { aptosClient } from "./client-providers";
 
 export default function AptosCoreProvider({ children }: PropsWithChildren) {
     const wallet = useWallet();
-    const defaultNetwork: NetworkInfo = {
-        network:
-            ENVIRONMENT === Environment.Production
-                ? Network.MAINNET
-                : Network.TESTNET,
-    };
-    const core = useWalletAdapterCore({ wallet, defaultNetwork });
+    const core = useWalletAdapterCore({
+        wallet,
+        config: { apiKey: APTOS_CLIENT_API_KEY, aptos: aptosClient },
+    });
     return <AptosJSCoreProvider core={core}>{children}</AptosJSCoreProvider>;
 }
