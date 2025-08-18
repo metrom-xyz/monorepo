4005d78122163655526d1d652468afed85ddb2db
Paolo Guerra <pg.guerrap@proton.me>
2025-08-18 18:59:12 +0200

    fix(frontend): remove duplicated env

--- packages/frontend/src/commons/env.ts
+++ packages/frontend/src/commons/env.ts
@@ -17,10 +17,6 @@ if (!SAFE && !WALLETCONNECT_PROJECT_ID)
     );
 
 export const FATHOM_SITE_ID: string = process.env.NEXT_PUBLIC_FATHOM_SITE_ID!;
-export const EXPERIMENTAL_CHAINS: number[] =
-    process.env.NEXT_PUBLIC_EXPERIMENTAL_CHAINS?.split(",")
-        .map((chain) => parseInt(chain.trim()))
-        .filter(Boolean) || [];
 export const APTOS: boolean = process.env.NEXT_PUBLIC_APTOS === "true";
 export const APTOS_CLIENT_API_KEY: string | undefined =
     process.env.NEXT_PUBLIC_APTOS_CLIENT_API_KEY;
