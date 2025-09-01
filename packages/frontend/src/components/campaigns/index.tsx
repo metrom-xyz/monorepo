ca47d3c452ec57580890195a425b5fe614536092
Paolo Guerra <pg.guerrap@proton.me>
2025-09-01 18:01:08 +0200

    feat(frontend): show aptos campaign on main campaigns list

--- packages/frontend/src/components/campaigns/index.tsx
+++ packages/frontend/src/components/campaigns/index.tsx
@@ -3,13 +3,14 @@
 import { useCampaigns } from "@/src/hooks/useCampaigns";
 import { ProjectOpportunitiesBanner } from "./project-opportunities-banner";
 import { CampaignsTable } from "../campaigns-table";
-import { CHAIN_TYPE } from "@/src/commons";
+import { APTOS } from "@/src/commons/env";
+import { ChainType } from "@metrom-xyz/sdk";
 
 import styles from "./styles.module.css";
 
 export function Campaigns() {
     const { loading: loadingCampaigns, campaigns } = useCampaigns({
-        chainType: CHAIN_TYPE,
+        chainType: APTOS ? ChainType.Aptos : undefined,
     });
 
     return (
