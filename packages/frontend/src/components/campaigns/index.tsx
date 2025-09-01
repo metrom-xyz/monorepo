8e1714ce879bf3e39a1c00b42c0173393c34f3c7
luzzif <federico.luzzi@proton.me>
2025-09-01 07:50:15 +0100

    feat(frontend): limit aptos frontend to only showing aptos campaigns

--- packages/frontend/src/components/campaigns/index.tsx
+++ packages/frontend/src/components/campaigns/index.tsx
@@ -3,11 +3,14 @@
 import { useCampaigns } from "@/src/hooks/useCampaigns";
 import { ProjectOpportunitiesBanner } from "./project-opportunities-banner";
 import { CampaignsTable } from "../campaigns-table";
+import { CHAIN_TYPE } from "@/src/commons";
 
 import styles from "./styles.module.css";
 
 export function Campaigns() {
-    const { loading: loadingCampaigns, campaigns } = useCampaigns();
+    const { loading: loadingCampaigns, campaigns } = useCampaigns({
+        chainType: CHAIN_TYPE,
+    });
 
     return (
         <div className={styles.root}>
