0104a55cbde186e3dd2f1b7d9c8c85f8780ea4ce
Paolo Guerra <pg.guerrap@proton.me>
2025-09-08 15:27:30 +0200

    fix(frontend): fix types

--- packages/frontend/src/types/chain.ts
+++ packages/frontend/src/types/chain.ts
@@ -2,9 +2,9 @@ import type { SupportedChain as SupportedChainMvm } from "@metrom-xyz/aptos-cont
 import type { SupportedChain as SupportedChainEvm } from "@metrom-xyz/contracts";
 import type { ChainType } from "@metrom-xyz/sdk";
 
+export type SupportedCrossVmChain = SupportedChainEvm | SupportedChainMvm;
+
 export interface ChainWithType {
     id: number;
     type: ChainType;
 }
-
-export type SupportedCrossVmChain = SupportedChainEvm | SupportedChainMvm;
