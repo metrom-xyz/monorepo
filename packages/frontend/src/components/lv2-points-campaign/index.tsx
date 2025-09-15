2c9bd55b41d3359c014f2fd9917d6e4b17400123
Paolo Guerra <pg.guerrap@proton.me>
2025-09-15 17:52:23 +0200

    feat(frontend): add metrom enso shortcuts widget

--- packages/frontend/src/components/lv2-points-campaign/index.tsx
+++ packages/frontend/src/components/lv2-points-campaign/index.tsx
@@ -4,13 +4,14 @@ import { Details } from "./details";
 import { Header } from "./header";
 import { Leaderboard } from "../leaderboard";
 import { SupportedLiquityV2 } from "@metrom-xyz/sdk";
-import { ENVIRONMENT } from "@/src/commons/env";
+import { ENSO_FINANCE_API_KEY, ENVIRONMENT } from "@/src/commons/env";
 import { useLv2PointsCampaignLeaderboard } from "@/src/hooks/useLv2PointsCampaignLeaderboard";
 import { LV2_POINTS_CAMPAIGNS } from "@/src/commons/lv2-points";
 import { Actions } from "./actions";
 import { ProjectIntro } from "../protocol-intro";
 import { useTranslations } from "next-intl";
-// import { Widget } from "@metrom-xyz/enso-shortcuts-widget";
+import { Widget } from "@metrom-xyz/enso-shortcuts-widget";
+import { useAppKit } from "@reown/appkit/react";
 
 import styles from "./styles.module.css";
 
@@ -21,6 +22,7 @@ interface Lv2PointsCampaignProps {
 export function Lv2PointsCampaign({ protocol }: Lv2PointsCampaignProps) {
     const t = useTranslations("lv2PointsCampaignPage");
 
+    const { open } = useAppKit();
     const { loading: loadingLeaderboard, leaderboard } =
         useLv2PointsCampaignLeaderboard({ protocol });
 
@@ -43,6 +45,10 @@ export function Lv2PointsCampaign({ protocol }: Lv2PointsCampaignProps) {
         actions,
     } = campaign;
 
+    async function handleOnConnect() {
+        await open();
+    }
+
     return (
         <div className={styles.root}>
             <Header
@@ -53,15 +59,16 @@ export function Lv2PointsCampaign({ protocol }: Lv2PointsCampaignProps) {
                 icon={icon}
             />
             <Details from={from} to={to} protocol={name} />
-            {/* {protocol === SupportedLiquityV2.Ebisu && (
+            {protocol === SupportedLiquityV2.Ebisu && (
                 <Widget
                     apiKey={ENSO_FINANCE_API_KEY}
                     tokenIn="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
                     chainId={1}
                     tokenOut="0x2b4b2a06c0fdebd8de1545abdffa64ec26416796"
                     outChainId={1}
+                    onConnectWallet={handleOnConnect}
                 />
-            )} */}
+            )}
             {protocolIntro && (
                 <ProjectIntro
                     project={protocol}
