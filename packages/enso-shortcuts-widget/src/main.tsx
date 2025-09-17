529ca63f872e71af36f364477f8c761d38cc7de1
Paolo Guerra <pg.guerrap@proton.me>
2025-09-17 18:08:42 +0200

    feat(enso-shortcuts-widget): add dev app to test widget

---
+++ packages/enso-shortcuts-widget/src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { App } from "./app";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import "./index.css";
+
+const RAINBOWKIT_PROJECT_ID = import.meta.env.VITE_RAINBOWKIT_PROJECT_ID ?? "";
+
+const config = getDefaultConfig({
+    appName: "My RainbowKit App",
+    projectId: RAINBOWKIT_PROJECT_ID,
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+});
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                    <App />
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    </StrictMode>,
+);
