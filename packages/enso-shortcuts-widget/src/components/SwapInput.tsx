99f9e0b8ccc8e48b232a90c69e1f398f0740b6b7
Paolo Guerra <pg.guerrap@proton.me>
2025-09-25 18:13:44 +0200

    fix(enso-shortcuts-widget): fix mobile issue, improve UI

--- packages/enso-shortcuts-widget/src/components/SwapInput.tsx
+++ packages/enso-shortcuts-widget/src/components/SwapInput.tsx
@@ -84,66 +84,64 @@ const SwapInput = ({
     }
 
     return (
-        <div className="w-full content-center rounded-xl">
-            <div className="w-full flex flex-col gap-2 p-2 rounded-2xl theme-surface-2 min-h-[6.5rem]">
-                <div className="flex w-full gap-2">
-                    <TokenSelector
-                        project={project}
-                        projectsFilter={projects}
-                        setChainId={setChainId}
-                        chainId={chainId}
-                        limitTokens={limitTokens}
-                        excludeTokens={excludeTokens}
-                        obligatedToken={obligatedToken}
-                        portalRef={portalRef}
-                        value={tokenValue}
-                        onChange={tokenOnChange}
-                    />
+        <div className="w-full flex flex-col gap-2 p-2 rounded-2xl border border-zinc-500 min-h-[6.5rem]">
+            <div className="flex w-full gap-2">
+                <TokenSelector
+                    project={project}
+                    projectsFilter={projects}
+                    setChainId={setChainId}
+                    chainId={chainId}
+                    limitTokens={limitTokens}
+                    excludeTokens={excludeTokens}
+                    obligatedToken={obligatedToken}
+                    portalRef={portalRef}
+                    value={tokenValue}
+                    onChange={tokenOnChange}
+                />
 
-                    <div className="flex w-full h-full justify-end items-center self-center">
-                        {loading ? (
-                            <div className="h-10 w-40 theme-surface rounded-md animate-pulse" />
-                        ) : (
-                            <TextInput
-                                type="number"
-                                size="lg"
-                                disabled={disabled}
-                                placeholder="0.0"
-                                value={tempInputValue}
-                                onChange={handleInputOnChange}
-                                className="w-full min-w-44 [&>div>input]:h-16 [&>div>input]:text-right [&>div>input]:text-[1.75rem]"
-                            />
-                        )}
+                <div className="flex w-full h-full justify-end items-center self-center">
+                    {loading ? (
+                        <div className="h-10 w-full max-w-40 theme-surface-2 rounded-md animate-pulse" />
+                    ) : (
+                        <TextInput
+                            type="number"
+                            size="lg"
+                            disabled={disabled}
+                            placeholder="0.0"
+                            value={tempInputValue}
+                            onChange={handleInputOnChange}
+                            className="w-full max-w-44 [&>div>input]:h-16 [&>div>input]:text-right [&>div>input]:text-[1.75rem] [&>div]:bg-transparent! [&>div>input]:bg-transparent!"
+                        />
+                    )}
+                </div>
+            </div>
+
+            <div className="flex justify-between items-center">
+                <div
+                    className={`flex items-center gap-2 ${address ? "visible" : "hidden"}`}
+                >
+                    <Typography weight="medium" size="sm" noWrap>
+                        {formatNumber(balanceValue)} {tokenInInfo?.symbol}
+                    </Typography>
+
+                    <div className="rounded-sm px-1 theme-surface theme-surface-2-hover hover:cursor-pointer transition-colors duration-200 ease-in-out">
+                        <Typography
+                            uppercase
+                            weight="medium"
+                            size="xs"
+                            onClick={handleMaxOnClick}
+                            className="text-[11px]!"
+                        >
+                            Max
+                        </Typography>
                     </div>
                 </div>
 
-                <div className="flex justify-between items-center">
-                    <div
-                        className={`flex items-center gap-2 ${address ? "visible" : "hidden"}`}
-                    >
-                        <Typography weight="medium" size="sm" noWrap>
-                            {formatNumber(balanceValue)} {tokenInInfo?.symbol}
-                        </Typography>
-
-                        <div className="rounded-sm px-1 theme-surface theme-surface-2-hover hover:cursor-pointer transition-colors duration-200 ease-in-out">
-                            <Typography
-                                uppercase
-                                weight="medium"
-                                size="xs"
-                                onClick={handleMaxOnClick}
-                                className="text-[11px]!"
-                            >
-                                Max
-                            </Typography>
-                        </div>
-                    </div>
-
-                    {usdValue ? (
-                        <Typography size="xs" weight="medium">
-                            ~{formatUSD(usdValue)}
-                        </Typography>
-                    ) : null}
-                </div>
+                {usdValue ? (
+                    <Typography size="xs" weight="medium">
+                        ~{formatUSD(usdValue)}
+                    </Typography>
+                ) : null}
             </div>
         </div>
     );
