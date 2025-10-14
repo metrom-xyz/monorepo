9ff65600d4cb75784425b90b4091032ac9a86d17
Paolo Guerra <pg.guerrap@proton.me>
2025-10-14 12:17:07 +0200

    feat(frontend): use metrom API to fetch asset metadata

--- packages/sdk/src/client/types/fungible-asset.ts
+++ packages/sdk/src/client/types/fungible-asset.ts
@@ -1,6 +1,8 @@
+import type { Address } from "viem";
 import type { BackendErc20Token } from "./commons";
 
 export interface BackendFungibleAsset extends BackendErc20Token {
+    address: Address;
     totalSupply: string;
     usdTotalSupply: number;
 }
