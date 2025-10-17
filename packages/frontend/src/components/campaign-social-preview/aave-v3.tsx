4b479878d7ac345d38a3e06c96c60a3f872347e5
Paolo Guerra <pg.guerrap@proton.me>
2025-10-17 11:49:55 +0200

    fix(frontend/sdk): fix opengraph image generation, fix campaign filters

--- packages/frontend/src/components/campaign-social-preview/aave-v3.tsx
+++ packages/frontend/src/components/campaign-social-preview/aave-v3.tsx
@@ -1,19 +1,19 @@
-import type { AaveV3Collateral } from "@metrom-xyz/sdk";
 import { RemoteLogo } from "./remote-logo";
 import type { Address } from "viem";
 
 interface AaveV3Props {
-    collateral: AaveV3Collateral;
+    address: Address;
+    symbol: string;
     tokenUris: Record<Address, string>;
 }
 
-export function AaveV3({ collateral, tokenUris }: AaveV3Props) {
+export function AaveV3({ address, symbol, tokenUris }: AaveV3Props) {
     return (
         <div tw="flex" style={{ gap: 18 }}>
             <div tw="flex">
-                <RemoteLogo src={tokenUris[collateral.token.address]} />
+                <RemoteLogo src={tokenUris[address]} />
             </div>
-            <span tw="text-[42px]">{collateral.token.symbol}</span>
+            <span tw="text-[42px]">{symbol}</span>
         </div>
     );
 }
