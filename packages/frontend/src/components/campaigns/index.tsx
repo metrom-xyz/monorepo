d661c051494f0ba867b497b0ea152ff3fb23613f
Paolo Guerra <pg.guerrap@proton.me>
2025-10-23 16:11:51 +0200

    feat(frontend): add tabs to campaigns page, start adapting new designs

--- packages/frontend/src/components/campaigns/index.tsx
+++ packages/frontend/src/components/campaigns/index.tsx
@@ -2,14 +2,34 @@
 
 import { ProjectOpportunitiesBanner } from "./project-opportunities-banner";
 import { CampaignsTable } from "../campaigns-table";
+import { useState } from "react";
+import { BackendCampaignType } from "@metrom-xyz/sdk";
+import { Tab, Tabs } from "@metrom-xyz/ui";
+import { TokensIcon } from "@/src/assets/tokens-icon";
+import { PointsIcon } from "@/src/assets/points-icon";
+import { useTranslations } from "next-intl";
 
 import styles from "./styles.module.css";
 
 export function Campaigns() {
+    const t = useTranslations("allCampaigns");
+
+    const [type, setType] = useState<BackendCampaignType>(
+        BackendCampaignType.Rewards,
+    );
+
     return (
         <div className={styles.root}>
             <ProjectOpportunitiesBanner />
-            <CampaignsTable />
+            <Tabs size="xl" value={type} onChange={setType}>
+                <Tab icon={TokensIcon} value={BackendCampaignType.Rewards}>
+                    {t("tabs.tokens")}
+                </Tab>
+                <Tab icon={PointsIcon} value={BackendCampaignType.Points}>
+                    {t("tabs.points")}
+                </Tab>
+            </Tabs>
+            <CampaignsTable type={type} />
         </div>
     );
 }
