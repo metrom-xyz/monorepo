1c4c7b29f67a9719dc4f102077673e0f4cda2636
Paolo Guerra <pg.guerrap@proton.me>
2025-11-04 14:53:44 +0100

    fix(react): export missing types

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,7 +2,14 @@ export { MetromProvider } from "./context/metrom-provider";
 export {
     Environment,
     SupportedDex,
+    SupportedAaveV3,
+    SupportedGmxV1,
+    SupportedLiquityV2,
+    SupportedBridge,
+    BackendCampaignType as CampaignType,
     BackendCampaignStatus as CampaignStatus,
+    BackendCampaignOrderBy as CampaignOrderBy,
+    type SupportedProtocol,
     Campaign,
     ChainType,
 } from "@metrom-xyz/sdk";
