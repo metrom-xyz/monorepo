2a8761d3a60fb28bb59b071a197ff31c1034bdf2
Paolo Guerra <pg.guerrap@proton.me>
2025-11-05 15:41:18 +0100

    feat(frontend): move filter options to hook, add type to url params

--- packages/frontend/src/components/campaigns/index.tsx
+++ packages/frontend/src/components/campaigns/index.tsx
@@ -7,9 +7,11 @@ import { useTranslations } from "next-intl";
 import { TokensIcon } from "@/src/assets/tokens-icon";
 import { PointsIcon } from "@/src/assets/points-icon";
 import { BackendCampaignType } from "@metrom-xyz/sdk";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProjectsList } from "../projects-list";
 import { ProjectsIcon } from "@/src/assets/projects-icon";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { SkeletonCampaigns } from "./skeleton-campaigns";
 
 import styles from "./styles.module.css";
 
@@ -17,10 +19,53 @@ export type BackendCampaignTypeAndProjects = BackendCampaignType | "projects";
 
 export function Campaigns() {
     const t = useTranslations("allCampaigns");
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
 
-    const [type, setType] = useState<BackendCampaignTypeAndProjects>(
-        BackendCampaignType.Rewards,
-    );
+    const [type, setType] = useState<BackendCampaignTypeAndProjects>();
+
+    // This hooks initializes the type state with the values coming from the url param, or
+    // if it's missing it defaults to rewards and adds that to the url.
+    useEffect(() => {
+        if (type) return;
+
+        const params = new URLSearchParams(searchParams.toString());
+        const urlType = params.get("type");
+
+        if (
+            urlType &&
+            urlType !== BackendCampaignType.Points &&
+            urlType !== BackendCampaignType.Rewards &&
+            urlType !== "projects"
+        ) {
+            setType(BackendCampaignType.Rewards);
+            return;
+        }
+
+        if (urlType) {
+            setType(urlType as BackendCampaignTypeAndProjects);
+            return;
+        }
+
+        params.set("type", BackendCampaignType.Rewards);
+        router.replace(`${pathname}?${params.toString()}`, {
+            scroll: false,
+        });
+        setType(BackendCampaignType.Rewards);
+    }, [pathname, router, searchParams, type]);
+
+    useEffect(() => {
+        if (!type) return;
+
+        const params = new URLSearchParams(searchParams.toString());
+        params.set("type", type);
+        router.replace(`${pathname}?${params.toString()}`, {
+            scroll: false,
+        });
+    }, [pathname, router, searchParams, type]);
+
+    if (!type) return <SkeletonCampaigns type={BackendCampaignType.Rewards} />;
 
     return (
         <div className={styles.root}>
