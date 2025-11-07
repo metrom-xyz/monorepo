ae0b1424f3176eab1421397a706a9da7eb6e5995
Paolo Guerra <pg.guerrap@proton.me>
2025-11-07 17:16:42 +0100

    fix(chains): fix issues with svg linear gradients

--- packages/chains/src/types/common.ts
+++ packages/chains/src/types/common.ts
@@ -1,9 +1,3 @@
 import type { SVGProps } from "react";
 
-export enum Theme {
-    System = "system",
-    Dark = "dark",
-    Light = "light",
-}
-
 export type SVGIcon = Omit<SVGProps<SVGSVGElement>, "dangerouslySetInnerHTML">;
