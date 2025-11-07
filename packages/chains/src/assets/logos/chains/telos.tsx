f98036f4ddf299628ea2cdb6a58c91a618b1d585
Paolo Guerra <pg.guerrap@proton.me>
2025-11-07 17:16:42 +0100

    fix(chains): fix issues with svg linear gradients

--- packages/chains/src/assets/logos/chains/telos.tsx
+++ packages/chains/src/assets/logos/chains/telos.tsx
@@ -1,6 +1,9 @@
+import { useSvgIds } from "../../../hooks/useSvgIds";
 import { SVGIcon } from "../../../types/common";
 
 export function TelosLogo(props: SVGIcon) {
+    const svg = useSvgIds();
+
     return (
         <svg
             width="256"
@@ -13,12 +16,12 @@ export function TelosLogo(props: SVGIcon) {
             <g clipPath="url(#clip0_2065_15434)">
                 <path
                     d="M128 256C57.4254 256 0 198.575 0 128C0 57.4254 57.4254 0 128 0C198.575 0 256 57.4254 256 128C256 198.575 198.575 256 128 256ZM128 46.5455C83.0836 46.5455 46.5455 83.0836 46.5455 128C46.5455 172.916 83.0836 209.455 128 209.455C172.916 209.455 209.455 172.916 209.455 128C209.455 83.0836 172.916 46.5455 128 46.5455Z"
-                    fill="url(#paint0_linear_2065_15434)"
+                    fill={svg.url("a")}
                 />
             </g>
             <defs>
                 <linearGradient
-                    id="paint0_linear_2065_15434"
+                    id={svg.id("a")}
                     x1="34.2421"
                     y1="37.7301"
                     x2="205.363"
