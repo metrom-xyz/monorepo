9717505cc54d47fb9f79f8f389fea28fcec761bb
Paolo Guerra <pg.guerrap@proton.me>
2025-11-11 12:25:27 +0100

    feat(frontend): improve campaign action component, remove duplicated code

--- packages/frontend/src/components/campaigns/campaign/action/gmx-v1-liquidity.tsx
+++ packages/frontend/src/components/campaigns/campaign/action/gmx-v1-liquidity.tsx
@@ -1,9 +1,6 @@
 import { Typography } from "@metrom-xyz/ui";
-import { useTranslations } from "next-intl";
-import { RestrictionType, TargetType } from "@metrom-xyz/sdk";
+import { TargetType } from "@metrom-xyz/sdk";
 import { type TargetedNamedCampaign } from "@/src/types/campaign";
-
-import styles from "./styles.module.css";
 
 interface GmxV1LiquidityProps<T extends TargetType.GmxV1Liquidity> {
     campaign: TargetedNamedCampaign<T>;
@@ -12,29 +9,9 @@ interface GmxV1LiquidityProps<T extends TargetType.GmxV1Liquidity> {
 export function GmxV1Liquidity<T extends TargetType.GmxV1Liquidity>({
     campaign,
 }: GmxV1LiquidityProps<T>) {
-    const t = useTranslations("allCampaigns");
-
     return (
-        <div className={styles.root}>
-            <div className={styles.titleContainer}>
-                <Typography size="lg" weight="medium" truncate>
-                    {campaign.name}
-                </Typography>
-                {campaign.specification?.kpi && (
-                    <div className={styles.chip}>
-                        <Typography size="sm" weight="medium" uppercase>
-                            {t("kpi")}
-                        </Typography>
-                    </div>
-                )}
-                {campaign.restrictions?.type === RestrictionType.Whitelist && (
-                    <div className={styles.chip}>
-                        <Typography size="xs" weight="medium" uppercase>
-                            {t("restricted")}
-                        </Typography>
-                    </div>
-                )}
-            </div>
-        </div>
+        <Typography size="lg" weight="medium" truncate>
+            {campaign.name}
+        </Typography>
     );
 }
