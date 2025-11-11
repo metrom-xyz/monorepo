8aaaf7e95bb5b8f71aaa137b48a36e23b7cf3c47
Paolo Guerra <pg.guerrap@proton.me>
2025-11-11 12:25:27 +0100

    feat(frontend): improve campaign action component, remove duplicated code

--- packages/frontend/src/components/campaigns/campaign/action/hold-fungible-asset.tsx
+++ packages/frontend/src/components/campaigns/campaign/action/hold-fungible-asset.tsx
@@ -1,10 +1,7 @@
 import { Typography } from "@metrom-xyz/ui";
-import { useTranslations } from "next-intl";
-import { RestrictionType, TargetType } from "@metrom-xyz/sdk";
+import { TargetType } from "@metrom-xyz/sdk";
 import { type TargetedNamedCampaign } from "@/src/types/campaign";
 import { RemoteLogo } from "@/src/components/remote-logo";
-
-import styles from "./styles.module.css";
 
 interface HoldFungibleAssetProps<T extends TargetType.HoldFungibleAsset> {
     campaign: TargetedNamedCampaign<T>;
@@ -13,33 +10,15 @@ interface HoldFungibleAssetProps<T extends TargetType.HoldFungibleAsset> {
 export function HoldFungibleAsset<T extends TargetType.HoldFungibleAsset>({
     campaign,
 }: HoldFungibleAssetProps<T>) {
-    const t = useTranslations("allCampaigns");
-
     return (
-        <div className={styles.root}>
+        <>
             <RemoteLogo
                 address={campaign.target.asset.address}
                 chain={campaign.target.chainId}
             />
-            <div className={styles.titleContainer}>
-                <Typography size="lg" weight="medium" truncate>
-                    {campaign.name}
-                </Typography>
-                {campaign.specification?.kpi && (
-                    <div className={styles.chip}>
-                        <Typography size="sm" weight="medium" uppercase>
-                            {t("kpi")}
-                        </Typography>
-                    </div>
-                )}
-                {campaign.restrictions?.type === RestrictionType.Whitelist && (
-                    <div className={styles.chip}>
-                        <Typography size="xs" weight="medium" uppercase>
-                            {t("restricted")}
-                        </Typography>
-                    </div>
-                )}
-            </div>
-        </div>
+            <Typography size="lg" weight="medium" truncate>
+                {campaign.name}
+            </Typography>
+        </>
     );
 }
