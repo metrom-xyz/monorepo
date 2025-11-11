9d89d65dc658378f2c8ba6905889d2cc58cca726
Paolo Guerra <pg.guerrap@proton.me>
2025-11-11 15:15:32 +0100

    feat(frontend/ui): implement new activity feed

--- packages/frontend/src/components/campaigns/campaign/action/katana-vault.tsx
+++ packages/frontend/src/components/campaigns/campaign/action/katana-vault.tsx
@@ -2,21 +2,22 @@ import { Typography } from "@metrom-xyz/ui";
 import { TargetType } from "@metrom-xyz/sdk";
 import { type TargetedNamedCampaign } from "@/src/types/campaign";
 import { RemoteLogo } from "@/src/components/remote-logo";
+import type { ActionSizes } from ".";
 
-interface KatanaVaultProps<T extends TargetType.KatanaVault> {
+interface KatanaVaultProps<T extends TargetType.KatanaVault>
+    extends ActionSizes {
     campaign: TargetedNamedCampaign<T>;
 }
 
 export function KatanaVault<T extends TargetType.KatanaVault>({
+    nameSize,
+    logoSize,
     campaign,
 }: KatanaVaultProps<T>) {
     return (
         <>
-            <RemoteLogo
-                src={campaign.target.vaultIconUrl}
-                className="rounded-full!"
-            />
-            <Typography size="lg" weight="medium" truncate>
+            <RemoteLogo size={logoSize} src={campaign.target.vaultIconUrl} />
+            <Typography size={nameSize} weight="medium" truncate>
                 {campaign.name}
             </Typography>
         </>
