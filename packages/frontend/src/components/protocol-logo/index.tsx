9f0b4783b093d6a790c90b900dd5a1fc90b21c08
Paolo Guerra <pg.guerrap@proton.me>
2025-11-11 17:06:22 +0100

    fix(chains/frontend): update orki logos, fix protocol logo component

--- packages/frontend/src/components/protocol-logo/index.tsx
+++ packages/frontend/src/components/protocol-logo/index.tsx
@@ -1,16 +1,13 @@
 import { type ProtocolBase } from "@metrom-xyz/chains";
 import { useTheme } from "next-themes";
-import { SupportedLiquityV2 } from "@metrom-xyz/sdk";
-import { Theme } from "@metrom-xyz/ui";
+import { Theme, type RemoteLogoSize } from "@metrom-xyz/ui";
 
 import styles from "./styles.module.css";
 import classNames from "classnames";
 
-const SCALE_PROTOCOLS_LOGO: string[] = [SupportedLiquityV2.Orki];
-
 export interface ProtocolLogoProps {
     protocol?: ProtocolBase;
-    size?: "base" | "sm" | "md" | "lg";
+    size?: RemoteLogoSize;
     className?: string;
 }
 
@@ -29,9 +26,6 @@ export function ProtocolLogo({
             <LogoLight
                 className={classNames(styles.icon, className, {
                     [styles[size]]: true,
-                    [styles.iconBig]: SCALE_PROTOCOLS_LOGO.includes(
-                        protocol.slug,
-                    ),
                 })}
             />
         );
@@ -40,9 +34,6 @@ export function ProtocolLogo({
             <Logo
                 className={classNames(styles.icon, className, {
                     [styles[size]]: true,
-                    [styles.iconBig]: SCALE_PROTOCOLS_LOGO.includes(
-                        protocol.slug,
-                    ),
                 })}
             />
         );
