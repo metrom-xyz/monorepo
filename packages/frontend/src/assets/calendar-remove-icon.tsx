ad3db6370bfcf62bfa59e5660b2e3d067b2c467e
Paolo Guerra <pg.guerrap@proton.me>
2025-11-11 16:29:43 +0100

    feat(frontend): add empty activities section

---
+++ packages/frontend/src/assets/calendar-remove-icon.tsx
@@ -0,0 +1,71 @@
+import type { SVGIcon } from "../types/common";
+
+export function CalendarRemoveIcon(props: SVGIcon) {
+    return (
+        <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M6.66663 1.66669V4.16669"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M13.3334 1.66669V4.16669"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M2.91663 7.57495H17.0833"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M15 19.1667C16.8409 19.1667 18.3333 17.6743 18.3333 15.8333C18.3333 13.9924 16.8409 12.5 15 12.5C13.159 12.5 11.6666 13.9924 11.6666 15.8333C11.6666 17.6743 13.159 19.1667 15 19.1667Z"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M15.8917 16.7583L14.125 15"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M15.875 15.0167L14.1083 16.7834"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M17.5 7.08335V13.6333C16.8917 12.9417 16 12.5 15 12.5C13.1583 12.5 11.6667 13.9917 11.6667 15.8334C11.6667 16.4584 11.8417 17.05 12.15 17.55C12.325 17.85 12.55 18.1167 12.8083 18.3334H6.66667C3.75 18.3334 2.5 16.6667 2.5 14.1667V7.08335C2.5 4.58335 3.75 2.91669 6.66667 2.91669H13.3333C16.25 2.91669 17.5 4.58335 17.5 7.08335Z"
+                stroke="currentColor"
+                strokeWidth="1.04167"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+}
