7b5ece39290180cdb1982764431bdfd7c88c49d7
Paolo Guerra <pg.guerrap@proton.me>
2025-11-11 15:15:32 +0100

    feat(frontend/ui): implement new activity feed

--- packages/frontend/src/components/campaigns/campaign/action/aave-v3.tsx
+++ packages/frontend/src/components/campaigns/campaign/action/aave-v3.tsx
@@ -2,21 +2,25 @@ import { Typography } from "@metrom-xyz/ui";
 import { type AaveV3TargetType } from "@metrom-xyz/sdk";
 import { type TargetedNamedCampaign } from "@/src/types/campaign";
 import { RemoteLogo } from "@/src/components/remote-logo";
+import type { ActionSizes } from ".";
 
-interface AaveV3Props<T extends AaveV3TargetType> {
+interface AaveV3Props<T extends AaveV3TargetType> extends ActionSizes {
     campaign: TargetedNamedCampaign<T>;
 }
 
 export function AaveV3<T extends AaveV3TargetType>({
     campaign,
+    nameSize,
+    logoSize,
 }: AaveV3Props<T>) {
     return (
         <>
             <RemoteLogo
+                size={logoSize}
                 address={campaign.target.collateral.address}
                 chain={campaign.target.chainId}
             />
-            <Typography size="lg" weight="medium" truncate>
+            <Typography size={nameSize} weight="medium" noWrap>
                 {campaign.name}
             </Typography>
         </>
