ff64a7eee7a5fa81cbc9cd4b279e9c55172668c2
Paolo Guerra <pg.guerrap@proton.me>
2025-11-12 14:27:24 +0100

    feat(frontend): add skeleton project server side loading

--- packages/frontend/src/app/[locale]/projects/[project]/page.tsx
+++ packages/frontend/src/app/[locale]/projects/[project]/page.tsx
@@ -3,7 +3,6 @@ import { Project } from "@/src/components/project";
 import { routing, type Locale } from "@/src/i18n/routing";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
-import { connection } from "next/server";
 
 export interface Params {
     project: string;
@@ -35,7 +34,6 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
 
     setRequestLocale(locale);
 
-    await connection();
     return <Project project={project} />;
 }
 
