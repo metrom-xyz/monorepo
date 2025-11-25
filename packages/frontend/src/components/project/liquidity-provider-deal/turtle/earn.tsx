ad6f0888a3e201e4e39e480f079bc441f4d78040
Paolo Guerra <pg.guerrap@proton.me>
2025-11-25 16:59:04 +0100

    feat(frontend): integrate new Turtle earn widget, keep it disabled

--- packages/frontend/src/components/project/liquidity-provider-deal/turtle/earn.tsx
+++ packages/frontend/src/components/project/liquidity-provider-deal/turtle/earn.tsx
@@ -1,29 +1,44 @@
 "use client";
 
 import { TURTLE_REFERRAL_CODE } from "@/src/commons";
-import { useAppKit } from "@reown/appkit/react";
-import { EarnPage, useWagmiAdapter } from "@turtledev/react";
+import { useEarnWagmiAdapter } from "@/src/hooks/useEarnWagmiAdapter";
+import dynamic from "next/dynamic";
+import classNames from "classnames";
 
 import styles from "./styles.module.css";
 
-interface EarnProps {
-    campaignId?: string;
-    address?: string;
-}
+const EarnWidgetDynamic = dynamic(
+    () => import("@turtleclub/earn-widget").then((mod) => mod.EarnWidget),
+    {
+        loading: () => <SkeletonEarn />,
+        ssr: false,
+    },
+);
 
-export function Earn({ campaignId, address }: EarnProps) {
-    const { open } = useAppKit();
-    const adapter = useWagmiAdapter();
+export function Earn() {
+    const adapter = useEarnWagmiAdapter();
 
     return (
         <div className={styles.earn}>
-            <EarnPage
-                referral={TURTLE_REFERRAL_CODE}
-                campaignId={campaignId}
-                user={address}
-                openConnectionModal={open}
-                {...adapter}
+            <EarnWidgetDynamic
+                config={{
+                    theme: "dark",
+                    styles: {
+                        background: "#f9fafb",
+                        background_dark: "#171717",
+                        primary: "#0ea400",
+                        primary_dark: "#6cff95",
+                        foreground: "#101828",
+                        foreground_dark: "#f5f5f5",
+                    },
+                }}
+                adapter={adapter}
+                distributorId={TURTLE_REFERRAL_CODE}
             />
         </div>
     );
+}
+
+export function SkeletonEarn() {
+    return <div className={classNames(styles.earn, styles.loading)}></div>;
 }
