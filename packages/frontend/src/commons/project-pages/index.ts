28796ccb1917926d4b62b556eb3950112ec2c7fe
Paolo Guerra <pg.guerrap@proton.me>
2025-11-25 17:18:40 +0100

    feat(frontend): remove Lens chain project page

--- packages/frontend/src/commons/project-pages/index.ts
+++ packages/frontend/src/commons/project-pages/index.ts
@@ -1,39 +1,7 @@
 import type { ProjectPage } from "@/src/types/project-page-";
-import { LensLogoDark, SupportedChain } from "@metrom-xyz/chains";
-import { ChainType, Environment } from "@metrom-xyz/sdk";
+import { Environment } from "@metrom-xyz/sdk";
 
 export const PROJECT_PAGES: Record<Environment, Record<string, ProjectPage>> = {
     [Environment.Development]: {},
-    [Environment.Production]: {
-        lens: {
-            chain: {
-                id: SupportedChain.Lens,
-                type: ChainType.Evm,
-            },
-            name: "Lens",
-            description:
-                "Lens is a high performance L2 leveraging ZKsync, Avail & Ethereum's security. Purpose-built for SocialFi, it offers modular social primitives, quick settlement & decentralized storage.",
-            url: "https://lens.xyz",
-            brand: {
-                main: "#999696",
-                light: "#cdcdcd",
-            },
-            intro: {
-                articles: [
-                    {
-                        title: "Introducing the New Lens",
-                        href: "https://lens.xyz/news/introducing-the-new-lens",
-                    },
-                    {
-                        title: "GHO: Stablecoin as Gas on Lens Chain",
-                        href: "https://lens.xyz/news/gho-stablecoin-as-gas-on-lens-chain",
-                    },
-                ],
-            },
-            icon: LensLogoDark,
-            campaignsFilters: {
-                chainId: SupportedChain.Lens,
-            },
-        },
-    },
+    [Environment.Production]: {},
 };
