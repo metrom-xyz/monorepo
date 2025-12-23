f6061261e268fd97b908f6ee6a14ec9020bc30ab
Paolo Guerra <pg.guerrap@proton.me>
2025-12-23 14:47:46 +0100

    fix(chains): add ebisu to protocols on plasma

--- packages/chains/src/chains/plasma.ts
+++ packages/chains/src/chains/plasma.ts
@@ -1,8 +1,8 @@
-import { BaseCampaignType, ChainType, SupportedDex } from "@metrom-xyz/sdk";
+import { BaseCampaignType, ChainType, SupportedDex, SupportedLiquityV2, TargetType } from "@metrom-xyz/sdk";
 import { SupportedChain, ADDRESS } from "@metrom-xyz/contracts";
 import { plasma } from "viem/chains";
 import type { ChainData } from "../types/chains";
-import { BalancerLogo, LithosLogo, PlasmaLogo } from "../assets";
+import { BalancerLogo, EbisuLogo, LithosLogo, PlasmaLogo } from "../assets";
 import { DepositUrlType, ProtocolType } from "../types/protocol";
 
 export const plasmaData: ChainData = {
@@ -46,6 +46,23 @@ export const plasmaData: ChainData = {
                     "https://lithos.to/pools/deposit?token0={token_0}&token1={token_1}",
             },
             supportsFetchAllPools: true,
+        },
+        {
+            active: false,
+            type: ProtocolType.LiquityV2,
+            slug: SupportedLiquityV2.Ebisu,
+            logo: EbisuLogo,
+            name: "Ebisu",
+            debtToken: {
+                address: "0xef7b1a03e0897c33b63159e38d779e3970c0e2fc",
+                decimals: 18,
+                name: "ebUSD",
+                symbol: "ebUSD",
+            },
+            actionUrls: {
+                [TargetType.LiquityV2Debt]: "https://ebisu.money/borrow",
+                [TargetType.LiquityV2StabilityPool]: "https://ebisu.money/earn",
+            },
         },
     ],
     baseTokens: [],
