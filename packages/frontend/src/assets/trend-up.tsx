8f4f8effa5119a85cd1183dd7fb9b570daa25a5e
Paolo Guerra <pg.guerrap@proton.me>
2026-01-12 18:01:31 +0100

    feat(frontend): update trend icons

--- packages/frontend/src/assets/trend-up.tsx
+++ packages/frontend/src/assets/trend-up.tsx
@@ -3,21 +3,21 @@ import type { SVGIcon } from "../types/common";
 export function TrendUpIcon(props: SVGIcon) {
     return (
         <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
+            width="7"
+            height="5"
+            viewBox="0 0 7 5"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
             <path
-                d="M11 6.3335L8.2 9.1335L7.13333 7.5335L5 9.66683"
+                d="M6.5 0.5L3.7 3.3L2.63333 1.7L0.5 3.83333"
                 stroke="#0EA400"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
-                d="M9.66699 6.3335H11.0003V7.66683"
+                d="M5.16699 0.5H6.50033V1.83333"
                 stroke="#0EA400"
                 strokeLinecap="round"
                 strokeLinejoin="round"
