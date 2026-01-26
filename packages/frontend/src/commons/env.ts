9492e0625d1b025b822b7057da6cb9b36a24f50b
Paolo Guerra <pg.guerrap@proton.me>
2026-01-26 14:59:47 +0100

    fix(frontend): add thala to homepages

--- packages/frontend/src/commons/env.ts
+++ packages/frontend/src/commons/env.ts
@@ -2,7 +2,7 @@ import { Environment } from "@metrom-xyz/sdk";
 
 // export const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT as Environment;
 // FIXME: remove this once the tests for Thala are done
-export const ENVIRONMENT = Environment.Production;
+export const ENVIRONMENT = "production" as Environment;
 if (
     !ENVIRONMENT ||
     !(Object.values(Environment) as string[]).includes(ENVIRONMENT)
