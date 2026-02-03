5892172baf0118dd2a2ddd6fa76e57087e8320ef
luzzif <federico.luzzi@proton.me>
2026-02-03 15:17:32 +0700

    feat: initial odyssey subgraph implementation and upgrade graph cli

---
+++ subgraphs/odyssey/deployments.ts
@@ -0,0 +1,19 @@
+export interface Contract {
+    address: string;
+    startBlock: number;
+}
+
+export interface ChainConfig {
+    PositionRegistry: Contract;
+}
+
+export const DEPLOYMENTS: {
+    [network: string]: ChainConfig;
+} = {
+    base: {
+        PositionRegistry: {
+            address: "0xeE156D8ea7b96a5524CcC3CF9283ab85E80E9534",
+            startBlock: 25696510,
+        },
+    },
+};
