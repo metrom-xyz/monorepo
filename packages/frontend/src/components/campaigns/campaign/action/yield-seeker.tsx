0b640e7408e41b903233fce102ce06ba55ec745a
Paolo Guerra <pg.guerrap@proton.me>
2026-02-12 15:20:23 +0100

    feat(frontend): remove yieldseeker boost chip from campaign

--- packages/frontend/src/components/campaigns/campaign/action/yield-seeker.tsx
+++ packages/frontend/src/components/campaigns/campaign/action/yield-seeker.tsx
@@ -2,12 +2,6 @@ import type { TargetType } from "@metrom-xyz/sdk";
 import type { ActionSizes } from ".";
 import type { TargetedNamedCampaign } from "@/src/types/campaign";
 import { Typography } from "@metrom-xyz/ui";
-import { CampaignTag } from "@/src/components/campaign-tag";
-import { useTranslations } from "next-intl";
-import { formatPercentage } from "@/src/utils/format";
-import { YIELDSEEKER_BONUS_PERCENTAGE } from "@/src/commons";
-
-import styles from "./styles.module.css";
 
 interface YieldSeekerProps<T extends TargetType.YieldSeeker>
     extends ActionSizes {
@@ -18,24 +12,9 @@ export function YieldSeeker<T extends TargetType.YieldSeeker>({
     nameSize,
     campaign,
 }: YieldSeekerProps<T>) {
-    const t = useTranslations("allCampaigns.yieldSeeker");
-
     return (
-        <>
-            <Typography size={nameSize} weight="medium" truncate>
-                {campaign.name}
-            </Typography>
-            <div className={styles.tags}>
-                <div className={styles.yieldSeekerBoostTag}>
-                    <CampaignTag
-                        text={t("boost", {
-                            percentage: formatPercentage({
-                                percentage: YIELDSEEKER_BONUS_PERCENTAGE,
-                            }),
-                        })}
-                    />
-                </div>
-            </div>
-        </>
+        <Typography size={nameSize} weight="medium" truncate>
+            {campaign.name}
+        </Typography>
     );
 }
