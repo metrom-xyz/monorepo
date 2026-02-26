e29fce72572d6fdb1d1e791affb5fa795893a272
Paolo Guerra <pg.guerrap@proton.me>
2026-02-26 09:37:42 +0100

    fix(chains/frontend): hide ploutos campaigns

--- packages/chains/src/chains/arbitrum.ts
+++ packages/chains/src/chains/arbitrum.ts
@@ -60,12 +60,9 @@ export const arbitrumData: ChainData = {
                 },
             ],
             actionUrls: {
-                [TargetType.AaveV3Borrow]:
-                    "https://app.ploutos.money/?marketName=proto_arbitrum_v3",
-                [TargetType.AaveV3Supply]:
-                    "https://app.ploutos.money/?marketName=proto_arbitrum_v3",
-                [TargetType.AaveV3NetSupply]:
-                    "https://app.ploutos.money/?marketName=proto_arbitrum_v3",
+                [TargetType.AaveV3Borrow]: "",
+                [TargetType.AaveV3Supply]: "",
+                [TargetType.AaveV3NetSupply]: "",
                 // FIXME: this action is specific to aave-aptos
                 [TargetType.AaveV3BridgeAndSupply]: "",
             },
