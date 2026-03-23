57d2ff905987f7b0ce545466ee4909635b4ce991
luzzif <federico.luzzi@proton.me>
2026-03-23 16:25:13 +0000

    fix: fix stuff

--- packages/frontend/scripts/generate-supported-protocols-files.ts
+++ packages/frontend/scripts/generate-supported-protocols-files.ts
@@ -47,6 +47,7 @@ const LIQUITY_V2_HOMEPAGES: Record<SupportedLiquityV2, string> = {
 const AAVE_V3_HOMEPAGES: Record<SupportedAaveV3, string> = {
     [SupportedAaveV3.Aave]: "https://aave.com/",
     [SupportedAaveV3.Ploutos]: "https://app.ploutos.money/",
+    [SupportedAaveV3.Purrlend]: "https://app.purrlend.io/",
 };
 
 const PROTOCOL_CONFIGS = [
