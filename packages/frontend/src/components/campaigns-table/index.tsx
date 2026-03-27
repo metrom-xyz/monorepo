bac6be154b4a1893404c8d240f8582adcaaa700f
Paolo Guerra <pg.guerrap@proton.me>
2026-03-27 11:40:20 +0100

    fix(frontend): resolve todo

--- packages/frontend/src/components/campaigns-table/index.tsx
+++ packages/frontend/src/components/campaigns-table/index.tsx
@@ -10,12 +10,7 @@ import { CampaignRow, SkeletonCampaign } from "../campaigns/campaign";
 import { Filters, type FilterParams, type RawFilters } from "./filters";
 import { useCampaigns } from "@/src/hooks/useCampaigns";
 import { APTOS } from "@/src/commons/env";
-import {
-    BackendCampaignType,
-    ChainType,
-    SupportedAaveV3,
-    TargetType,
-} from "@metrom-xyz/sdk";
+import { BackendCampaignType, ChainType } from "@metrom-xyz/sdk";
 import { LoadingBar } from "../loading-bar";
 import { useDebounce, usePrevious } from "react-use";
 import { EmptyTable } from "./empty-table";
@@ -383,14 +378,6 @@ export function CampaignsTable({
                             <EmptyTable />
                         ) : (
                             campaigns?.map((campaign) => {
-                                // TODO: remove
-                                if (
-                                    campaign.target.type ===
-                                        TargetType.AaveV3NetSupply &&
-                                    campaign.target.brand.slug ===
-                                        SupportedAaveV3.Ploutos
-                                )
-                                    return null;
                                 return (
                                     <CampaignRow
                                         key={`${campaign.chainType}-${campaign.chainId}-${campaign.id}`}
