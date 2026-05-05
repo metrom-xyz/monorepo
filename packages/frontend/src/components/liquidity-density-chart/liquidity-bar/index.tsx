1cd609e6549a9e939b5cb384da7954921b4e6c1e
Paolo Guerra <pg.guerrap@proton.me>
2026-05-05 16:59:16 +0200

    fix(frontend): fix range chart styles for campaign details

--- packages/frontend/src/components/liquidity-density-chart/liquidity-bar/index.tsx
+++ packages/frontend/src/components/liquidity-density-chart/liquidity-bar/index.tsx
@@ -147,7 +147,7 @@ export function LiquidityBar({
                     {showPriceRange && closestFrom === idx && (
                         <text
                             x={x + 15}
-                            y={180}
+                            y={300}
                             textAnchor="end"
                             className={classNames(styles.label, styles.big)}
                         >
@@ -167,7 +167,7 @@ export function LiquidityBar({
                     {showPriceRange && closestTo === idx && (
                         <text
                             x={x}
-                            y={180}
+                            y={300}
                             textAnchor="start"
                             className={classNames(styles.label, styles.big)}
                         >
