b9778cde19ec08e2bb8a451cc9d68204869482af
Paolo Guerra <pg.guerrap@proton.me>
2026-05-11 15:07:48 +0200

    feat(frontend): update form button labels

--- packages/frontend/src/components/create-campaign/steps/campaign-rewards-step/index.tsx
+++ packages/frontend/src/components/create-campaign/steps/campaign-rewards-step/index.tsx
@@ -201,7 +201,7 @@ export function CampaignRewardsStep({
                 disabled={disabled || applyDisabled}
                 className={{ root: styles.button }}
             >
-                {t("saveRewards")}
+                {t("next")}
             </Button>
         </FormStep>
     );
