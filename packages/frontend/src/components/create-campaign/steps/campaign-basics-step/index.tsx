66a6dde5c0e15e03a5d758c657671a477b79fb28
Paolo Guerra <pg.guerrap@proton.me>
2026-05-11 15:07:48 +0200

    feat(frontend): update form button labels

--- packages/frontend/src/components/create-campaign/steps/campaign-basics-step/index.tsx
+++ packages/frontend/src/components/create-campaign/steps/campaign-basics-step/index.tsx
@@ -105,7 +105,7 @@ export function CampaignBasicsStep({
                 disabled={applyDisabled}
                 className={{ root: styles.button }}
             >
-                {t("saveBasics")}
+                {t("next")}
             </Button>
         </FormStep>
     );
