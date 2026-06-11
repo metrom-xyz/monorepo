682d18cf4dd625f16c041e0ced11c2b0bfdae770
Paolo Guerra <pg.guerrap@proton.me>
2026-06-11 17:09:28 +0200

    feat(chains/sdk/frontend): add support for orca amm on solana devnet

---
+++ packages/chains/src/chains/solana-devnet.ts
@@ -0,0 +1,59 @@
+import { PROGRAM_ID, SupportedChain } from "@metrom-xyz/programs-solana";
+import {
+    BaseCampaignType,
+    ChainType,
+    DistributablesType,
+    SupportedDex,
+} from "@metrom-xyz/sdk";
+import { ChainData } from "../types/chains";
+import { OrcaLogo, SolanaLogo } from "../assets";
+import { Address } from "viem";
+import { AmmPoolDepositUrlType, ProtocolType } from "../types/protocol";
+
+export const solanaDevelopmentData: ChainData = {
+    active: true,
+    id: SupportedChain.Devnet,
+    type: ChainType.Svm,
+    name: "Solana Devnet",
+    slug: "solana:devnet",
+    metromContract: {
+        address: PROGRAM_ID.toString() as Address,
+    },
+    blockExplorers: {
+        default: {
+            name: "Solana Explorer",
+            url: "https://solscan.io",
+        },
+    },
+    icon: SolanaLogo,
+    forms: [
+        {
+            active: true,
+            partner: false,
+            type: BaseCampaignType.AmmPoolLiquidity,
+            distributables: [DistributablesType.Tokens],
+        },
+        {
+            active: true,
+            partner: false,
+            type: BaseCampaignType.HoldFungibleAsset,
+            distributables: [DistributablesType.Tokens],
+        },
+    ],
+    protocols: [
+        {
+            active: true,
+            type: ProtocolType.Dex,
+            slug: SupportedDex.Orca,
+            logo: OrcaLogo,
+            name: "Orca",
+            depositUrl: {
+                type: AmmPoolDepositUrlType.QueryTokenAddresses,
+                template:
+                    "https://www.orca.so/pools?chainId=solanaDevnet&tokens={token_0}&tokens={token_1}",
+            },
+            supportsFetchAllPools: true,
+        },
+    ],
+    baseTokens: [],
+};
